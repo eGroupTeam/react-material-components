5e25781c1325f6e40f6a88bca77a81697b48c09e
SP_Penguin <abrcdf1023@gmail.com>
2019-01-15 16:50:23 +0800

    fixed wrong exec waring logic

--- src/PageLoader/PageLoader.jsx
+++ src/PageLoader/PageLoader.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-console.error(
-  `@e-group/react-material-components: PageLoader component will be deprecated in the next major release.
-  Please migrate to Loader component.`
-);
-
-const PageLoader = ({ classes }) => <div />;
+const PageLoader = ({ classes }) => {
+  console.error(
+    `@e-group/react-material-components: PageLoader component will be deprecated in the next major release.
+    Please migrate to Loader component.`
+  );
+  return <div />;
+};
 
 export default PageLoader;
