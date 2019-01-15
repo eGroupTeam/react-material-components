51f7af46c74554d396688cf1e3e74bbe3f8836d6
SP_Penguin <abrcdf1023@gmail.com>
2019-01-15 16:28:10 +0800

    add PageLoader deprecated warning

--- src/PageLoader/PageLoader.jsx
+++ src/PageLoader/PageLoader.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
+console.error(
+  `@e-group/react-material-components: PageLoader component will be deprecated in the next major release.
+  Please migrate to Loader component.`
+);
+
 const PageLoader = ({ classes }) => <div />;
 
 export default PageLoader;
