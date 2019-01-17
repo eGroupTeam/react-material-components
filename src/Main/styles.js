ade51b9a7ee8271ef76272356479fa143341675f
SP_Penguin <abrcdf1023@gmail.com>
2019-01-17 11:46:08 +0800

    fixed Main height issue

--- src/Main/styles.js
+++ src/Main/styles.js
@@ -1,7 +1,7 @@
 const styles = theme => ({
   root: {
     width: '100%',
-    height: '100%',
+    minHeight: '100%',
     padding: theme.spacing.unit * 3,
     paddingTop: theme.config.navbarHeight + theme.spacing.unit * 3
   }
