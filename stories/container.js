f27cd3d5c41f214060cce4776e874a0ca9912e71
SP_Penguin <abrcdf1023@gmail.com>
2019-02-14 15:50:16 +0800

    Container component done

--- stories/container.js
+++ stories/container.js
@@ -8,7 +8,18 @@ import appendStylesIntro from './utils/appendStylesIntro';
 
 storiesOf('Components', module).add(
   'Container',
-  () => <Container>content...</Container>,
+  () => (
+    <Container>
+      <div
+        style={{ border: '1px solid #000', height: '200px', padding: '5px' }}
+      >
+        Container's max width default is 960px.
+        <br />
+        If you want to change default value please config containerMaxWidth in
+        theme provider.
+      </div>
+    </Container>
+  ),
   {
     info: {
       text: appendStylesIntro(containerMarkdownText, styles),
