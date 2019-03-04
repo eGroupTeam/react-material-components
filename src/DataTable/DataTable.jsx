a215a10ac8e5887a4fb2c45ca5c791427c889770
SP_Penguin <abrcdf1023@gmail.com>
2019-03-04 15:07:10 +0800

    Fixed bug https://github.com/gregnb/mui-datatables/issues/267.

--- src/DataTable/DataTable.jsx
+++ src/DataTable/DataTable.jsx
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import MUIDataTable from 'mui-datatables';
 
 import Loader from '../Loader';
 
-const DataTable = ({ options, loading, ...rest }) => {
+const DataTable = ({
+  loading,
+  options: { onTableChange, ...otherOptions },
+  ...other
+}) => {
+  // TODO: Fixed bug https://github.com/gregnb/mui-datatables/issues/267.
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const handleTableChange = (action, tableState) => {
+    const { options } = this.props;
+    if (action === 'changeRowsPerPage') {
+      setRowsPerPage(tableState.rowsPerPage);
+    }
+    if (options && options.onTableChange) {
+      options.onTableChange(action, tableState);
+    }
+  };
+
   if (loading) return <Loader />;
   return (
     <MUIDataTable
-      {...rest}
       options={{
-        ...options
+        onTableChange: handleTableChange,
+        rowsPerPage,
+        ...otherOptions
       }}
+      {...other}
     />
   );
 };
