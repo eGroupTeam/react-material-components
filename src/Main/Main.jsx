a6d66f7b869c79298c176454a94373c70c25bbfe
SP_Penguin <abrcdf1023@gmail.com>
2019-03-07 14:22:11 +0800

    deprecated Main component

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import classNames from 'classnames';
 import withStyles from '@material-ui/core/styles/withStyles';
+import warning from 'warning';
 
 import styles from './styles';
 
-const Main = ({ classes, className, ...other }) => (
-  <main className={classNames(classes.root, className)} {...other} />
-);
+const Main = ({ classes, className, ...other }) => {
+  warning(
+    true,
+    'Main is been deprecated please use new component Container instead.'
+  );
+  return <main className={classNames(classes.root, className)} {...other} />;
+};
 
 export default withStyles(styles)(Main);
