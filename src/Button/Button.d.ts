a355297c4c6f64eee825241f39b51b280bb384de
SP_Penguin <abrcdf1023@gmail.com>
2019-03-13 18:07:07 +0800

    fixed Button types

--- src/Button/Button.d.ts
+++ src/Button/Button.d.ts
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { ButtonProps as MUIButtonProps } from '@material-ui/core/Button';
-import { ButtonProps as CircularProgressProps } from '@material-ui/core/CircularProgress';
+import { CircularProgressProps } from '@material-ui/core/CircularProgress';
 
 export interface ButtonProps {
   loading: boolean; 
