44ee39f66c36a57063405963572e93963b80144f
SP_Penguin <abrcdf1023@gmail.com>
2019-03-15 17:01:06 +0800

    refactor CheckboxField done

--- src/CheckboxField/CheckboxField.jsx
+++ src/CheckboxField/CheckboxField.jsx
@@ -1,22 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { fieldInputPropTypes, fieldMetaPropTypes } from 'redux-form';
 
 import Checkbox from '../Checkbox';
 
-export default class CheckboxField extends Component {
-  static propTypes = {
-    // redux form props
-    input: PropTypes.shape(fieldInputPropTypes).isRequired,
-    meta: PropTypes.shape(fieldMetaPropTypes).isRequired,
-    // customize props
-    MUICheckboxProps: PropTypes.object
-  };
+const CheckboxField = ({ input, meta, MUICheckboxProps, ...other }) => (
+  <Checkbox MUICheckboxProps={MUICheckboxProps} {...input} {...other} />
+);
 
-  render() {
-    const { input, meta, MUICheckboxProps, ...rest } = this.props;
-    return (
-      <Checkbox MUICheckboxProps={MUICheckboxProps} {...input} {...rest} />
-    );
-  }
-}
+CheckboxField.propTypes = {
+  // redux form props
+  input: PropTypes.shape(fieldInputPropTypes).isRequired,
+  meta: PropTypes.shape(fieldMetaPropTypes).isRequired,
+  // customize props
+  MUICheckboxProps: PropTypes.object
+};
+
+export default CheckboxField;
--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,5 @@
 export { default as Button } from './Button'
 export { default as Checkbox } from './Checkbox'
+export { default as CheckboxField } from './CheckboxField'
 export { default as DataTable } from './DataTable'
 export { default as AutoComplete } from './AutoComplete'
