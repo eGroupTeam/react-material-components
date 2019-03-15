b0ff11ca76810695a1cc10298251c5b3bd6b2f36
SP_Penguin <abrcdf1023@gmail.com>
2019-03-15 16:59:41 +0800

    refactor Checkbox and prepare story

--- src/Checkbox/Checkbox.jsx
+++ src/Checkbox/Checkbox.jsx
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import MUICheckbox from '@material-ui/core/Checkbox';
 
-export default class Checkbox extends Component {
-  static propTypes = {
-    // customize props
-    MUICheckboxProps: PropTypes.object
-  };
+const Checkbox = ({ control, MUICheckboxProps, ...other }) => {
+  return (
+    <FormControlLabel
+      control={<MUICheckbox {...MUICheckboxProps} />}
+      {...other}
+    />
+  );
+};
 
-  render() {
-    const { MUICheckboxProps, ...rest } = this.props;
-    return (
-      <FormControlLabel
-        control={<MUICheckbox {...MUICheckboxProps} />}
-        {...rest}
-      />
-    );
-  }
-}
+Checkbox.propTypes = {
+  // customize props
+  MUICheckboxProps: PropTypes.object
+};
+
+export default Checkbox;
--- src/index.d.ts
+++ src/index.d.ts
@@ -1,3 +1,4 @@
 export { default as Button } from './Button'
+export { default as Checkbox } from './Checkbox'
 export { default as DataTable } from './DataTable'
 export { default as AutoComplete } from './AutoComplete'
