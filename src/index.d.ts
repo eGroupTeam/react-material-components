bb4659f62a8a5509b2b022e1725639f2728b9075
SP_Penguin <abrcdf1023@gmail.com>
2019-03-15 18:59:24 +0800

    feat(CheckboxInput) add types file

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,6 @@
 export { default as Button } from './Button'
 export { default as Checkbox } from './Checkbox'
 export { default as CheckboxField } from './CheckboxField'
+export { default as CheckboxInput } from './CheckboxInput'
 export { default as DataTable } from './DataTable'
 export { default as AutoComplete } from './AutoComplete'
