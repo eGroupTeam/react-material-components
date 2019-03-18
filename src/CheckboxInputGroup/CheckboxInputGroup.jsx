796f4f7bf312b83aadcf58b303686009465e9732
SP_Penguin <abrcdf1023@gmail.com>
2019-03-18 11:08:20 +0800

    feat(CheckboxInputGroup) clear api and  comments

--- src/CheckboxInputGroup/CheckboxInputGroup.jsx
+++ src/CheckboxInputGroup/CheckboxInputGroup.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import warning from 'warning';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
 import FormGroup from '@material-ui/core/FormGroup';
@@ -8,15 +9,34 @@ import CheckboxInput from '../CheckboxInput';
 
 export default class CheckboxInputGroup extends Component {
   static propTypes = {
+    /**
+     * The content of the FormLabel.
+     */
     label: PropTypes.string,
-    options: PropTypes.array,
-    showHelperText: PropTypes.bool,
+    /**
+     * A shortcut for generate group items.
+     */
+    options: PropTypes.array.isRequired,
+    /**
+     * The content of the FormHelperText.
+     */
     helperText: PropTypes.string,
-    FormControlProps: PropTypes.object,
-    FormLabelProps: PropTypes.object,
-    FormGroupProps: PropTypes.object,
-    FormHelperTextProps: PropTypes.object,
-    children: PropTypes.node
+    /**
+     * To show helperText or not.
+     */
+    showHelperText: PropTypes.bool,
+    /**
+     * MUI FormLabel Props
+     */
+    MUIFormLabelProps: PropTypes.object,
+    /**
+     * MUI FormGroup Props
+     */
+    MUIFormGroupProps: PropTypes.object,
+    /**
+     * MUI FormHelperText Props
+     */
+    MUIFormHelperTextProps: PropTypes.object
   };
 
   render() {
@@ -25,20 +45,25 @@ export default class CheckboxInputGroup extends Component {
       options,
       showHelperText,
       helperText,
-      FormControlProps,
-      FormLabelProps,
+      MUIFormLabelProps,
       FormGroupProps,
       FormHelperTextProps,
-      children
+      children,
+      ...other
     } = this.props;
+
+    warning(
+      children === undefined,
+      'CheckboxInputGroup should not has children please use `options` only!'
+    );
+
     return (
-      <FormControl {...FormControlProps}>
-        <FormLabel {...FormLabelProps}>{label}</FormLabel>
+      <FormControl {...other}>
+        <FormLabel {...MUIFormLabelProps}>{label}</FormLabel>
         <FormGroup {...FormGroupProps}>
-          {children ||
-            options.map((option, index) => (
-              <CheckboxInput key={index} {...option} />
-            ))}
+          {options.map((option, index) => (
+            <CheckboxInput key={index} {...option} />
+          ))}
         </FormGroup>
         {showHelperText && (
           <FormHelperText {...FormHelperTextProps}>{helperText}</FormHelperText>
