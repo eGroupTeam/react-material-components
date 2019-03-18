7f7912ab2c466e556ea3dadbd087fa134e8a719c
SP_Penguin <abrcdf1023@gmail.com>
2019-03-18 11:09:11 +0800

    feat(storybook) CheckboxInputGroup update.

--- stories/checkboxInputGroup.stories.jsx
+++ stories/checkboxInputGroup.stories.jsx
@@ -4,45 +4,87 @@ import { storiesOf } from '@storybook/react';
 import checkboxInputGroupText from './doc/checkboxInputGroup.md';
 import CheckboxInputGroup from '../src/CheckboxInputGroup';
 
-storiesOf('CheckboxInputGroup', module).add(
-  'default',
-  () => (
-    <CheckboxInputGroup
-      label="default"
-      FormControlProps={{
-        margin: 'normal',
-        fullWidth: true,
-        required: true
-      }}
-      options={[
-        {
-          name: 'checkbox1',
-          label: 'normal checkbox',
-          MUICheckboxProps: {
-            color: 'primary'
-          }
-        },
-        {
-          name: 'checkbox2',
-          label: 'checked with text input',
-          MUICheckboxProps: {
-            color: 'primary'
+storiesOf('CheckboxInputGroup', module)
+  .add(
+    'default',
+    () => (
+      <CheckboxInputGroup
+        label="default"
+        options={[
+          {
+            name: 'checkbox1',
+            label: 'normal checkbox',
+            MUICheckboxProps: {
+              color: 'primary'
+            }
           },
-          toggleInput: true
-        },
-        {
-          name: 'checkbox3',
-          label: 'checked with text input',
-          toggleInput: true
-        }
-      ]}
-    />
-  ),
-  {
-    info: {
-      text: checkboxInputGroupText,
-      propTables: [CheckboxInputGroup],
-      propTablesExclude: [CheckboxInputGroup]
+          {
+            name: 'checkbox2',
+            label: 'checked with text input',
+            MUICheckboxProps: {
+              color: 'primary'
+            },
+            toggleInput: true
+          },
+          {
+            name: 'checkbox3',
+            label: 'checked with text input',
+            toggleInput: true
+          }
+        ]}
+        margin="normal"
+        fullWidth
+        required
+      />
+    ),
+    {
+      info: {
+        text: checkboxInputGroupText,
+        propTables: [CheckboxInputGroup],
+        propTablesExclude: [CheckboxInputGroup]
+      }
     }
-  }
-);
+  )
+  .add(
+    'with error helperText',
+    () => (
+      <CheckboxInputGroup
+        label="default"
+        options={[
+          {
+            name: 'checkbox1',
+            label: 'normal checkbox',
+            MUICheckboxProps: {
+              color: 'primary'
+            }
+          },
+          {
+            name: 'checkbox2',
+            label: 'checked with text input',
+            MUICheckboxProps: {
+              color: 'primary'
+            },
+            toggleInput: true
+          },
+          {
+            name: 'checkbox3',
+            label: 'checked with text input',
+            toggleInput: true
+          }
+        ]}
+        margin="normal"
+        fullWidth
+        required
+        showHelperText
+        error
+        helperText="helperText"
+      />
+    ),
+    {
+      info: {
+        text: checkboxInputGroupText,
+        propTables: [CheckboxInputGroup],
+        propTablesExclude: [CheckboxInputGroup]
+      }
+    }
+  );
