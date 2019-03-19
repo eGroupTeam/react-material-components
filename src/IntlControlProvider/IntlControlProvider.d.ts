f314471bef23a2c126bc62e9ebe5da81cf62d561
SP_Penguin <abrcdf1023@gmail.com>
2019-03-19 12:20:25 +0800

    update IntlControlProviderProps

--- src/IntlControlProvider/IntlControlProvider.d.ts
+++ src/IntlControlProvider/IntlControlProvider.d.ts
@@ -3,6 +3,7 @@ import * as React from 'react';
 export interface IntlControlProviderProps {
   onMount: Function; 
   onUpdateLocale: Function; 
+  messages: Object
 }
 
 declare const IntlControlProvider: React.ComponentType<IntlControlProviderProps>;
