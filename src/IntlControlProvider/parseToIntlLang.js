b085de4585b0d9e759370dba8cd2496cac639576
SP_Penguin <abrcdf1023@gmail.com>
2019-03-19 12:17:14 +0800

    fixed(parseToIntlLang) code style

--- src/IntlControlProvider/parseToIntlLang.js
+++ src/IntlControlProvider/parseToIntlLang.js
@@ -1,7 +1,5 @@
-const parseToIntlLang = lang => {
+export default function parseToIntlLang(lang) {
   if (lang === 'zh-tw') return 'zh';
   if (lang === 'en-us') return 'en';
   return lang;
-};
-
-export default parseToIntlLang;
+}
