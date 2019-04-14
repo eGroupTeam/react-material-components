3e1faba28f1ec2090044a971a6da9f7d34435693
SP_Penguin <abrcdf1023@gmail.com>
2019-04-14 16:08:52 +0800

    remove ErrorBoundry it's not the part of library

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ export { default as CheckboxInputGroup } from './CheckboxInputGroup';
 export { default as CheckboxInputGroupField } from './CheckboxInputGroupField';
 export { default as Container } from './Container';
 export { default as DataTable } from './DataTable';
-export { default as ErrorBoundry } from './ErrorBoundry';
 export { default as InputField } from './InputField';
 export { default as IntlControlProvider } from './IntlControlProvider';
 export { default as PageLoader } from './PageLoader';
