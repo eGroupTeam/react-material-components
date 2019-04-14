5f2e5ed317a20bd684c85c27a486e23429aab9e9
SP_Penguin <abrcdf1023@gmail.com>
2019-04-14 16:56:34 +0800

    rollback merge styles code into Button

--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -5,31 +5,7 @@ import withStyles from '@material-ui/core/styles/withStyles';
 import MuiButton from '@material-ui/core/Button/Button';
 import { CircularProgress } from '@material-ui/core';
 
-import green from '@material-ui/core/colors/green';
-
-const styles = theme => ({
-  root: {
-    display: 'inline-flex',
-    position: 'relative'
-  },
-  fullWidth: {
-    width: '100%'
-  },
-  success: {
-    backgroundColor: green[500],
-    '&:hover': {
-      backgroundColor: green[700]
-    }
-  },
-  progress: {
-    color: green[500],
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    marginTop: -12,
-    marginLeft: -12
-  }
-});
+import styles from './styles';
 
 export const ButtonComponent = ({
   classes,
