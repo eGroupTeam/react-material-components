d50b2d5f658e1886f788c1678861de56af6f3b1d
SP_Penguin <abrcdf1023@gmail.com>
2019-04-15 15:28:55 +0800

    doc(RadioGroup) update form UI

--- stories/radioGroup.stories.jsx
+++ stories/radioGroup.stories.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import ReduxForm from './components/ReduxForm';
+import Highlight from './components/Highlight';
+import Grid from '@material-ui/core/Grid';
+import { Field } from 'redux-form/immutable';
+import RadioGroup from '../src/RadioGroup';
+import RadioGroupField from '../src/RadioGroupField';
+
 import { storiesOf } from '@storybook/react';
 import { fromJS } from 'immutable';
 import { store } from './redux/configureStore';
-import { Provider } from 'react-redux';
-import ReduxForm from './components/ReduxForm';
-import { Field } from 'redux-form/immutable';
-
 import radioGroupText from './doc/radioGroup.md';
-import RadioGroup from '../src/RadioGroup';
-import RadioGroupField from '../src/RadioGroupField';
 
 storiesOf('RadioGroup', module)
   .addDecorator(story => <Provider store={store}>{story()}</Provider>)
@@ -79,35 +81,86 @@ storiesOf('RadioGroup', module)
   .add(
     'with Field',
     () => {
-      const initialValues = fromJS({
-        field1: '1'
-      });
-      return (
-        <ReduxForm initialValues={initialValues}>
-          <Field
-            name="field1"
-            component={RadioGroupField}
-            margin="normal"
-            fullWidth
-            required
-            label="with Field"
-            options={[
-              {
-                value: '1',
-                label: 'label1'
-              },
-              {
-                value: '2',
-                label: 'label2'
-              },
-              {
-                value: '3',
-                label: 'label3'
-              }
-            ]}
-          />
-        </ReduxForm>
-      );
+      const Form = () => {
+        const [values, setValues] = React.useState({
+          gender: 'male',
+          day: 'Monday'
+        });
+        const handleChange = values => {
+          setValues(values.toJS());
+        };
+        return (
+          <Grid container>
+            <Grid item xs={6}>
+              <ReduxForm onChange={handleChange} initialValues={fromJS(values)}>
+                <Field
+                  name="gender"
+                  component={RadioGroupField}
+                  margin="normal"
+                  fullWidth
+                  required
+                  label="gender"
+                  options={[
+                    {
+                      value: 'male',
+                      label: 'male'
+                    },
+                    {
+                      value: 'female',
+                      label: 'female'
+                    }
+                  ]}
+                />
+                <Field
+                  name="day"
+                  component={RadioGroupField}
+                  margin="normal"
+                  fullWidth
+                  required
+                  label="pick one day"
+                  options={[
+                    {
+                      value: 'Monday',
+                      label: 'Monday'
+                    },
+                    {
+                      value: 'Tuesday',
+                      label: 'Tuesday'
+                    },
+                    {
+                      value: 'Wednesday',
+                      label: 'Wednesday'
+                    },
+                    {
+                      value: 'Thursday',
+                      label: 'Thursday'
+                    },
+                    {
+                      value: 'Friday',
+                      label: 'Friday'
+                    },
+                    {
+                      value: 'Saturday',
+                      label: 'Saturday'
+                    },
+                    {
+                      value: 'Sunday',
+                      label: 'Sunday'
+                    }
+                  ]}
+                />
+              </ReduxForm>
+            </Grid>
+            <Grid item xs={6}>
+              <Highlight
+                code={JSON.stringify(values, null, 4)}
+                type="language-json"
+              />
+            </Grid>
+          </Grid>
+        );
+      };
+      return <Form />;
     },
     {
       notes: radioGroupText,
