1d372a9cd6b454470c2a6d6df3e5104ba70b3bc6
SP_Penguin <abrcdf1023@gmail.com>
2019-04-15 15:22:32 +0800

    doc(Radio) update form UI

--- stories/radio.stories.jsx
+++ stories/radio.stories.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { fromJS } from 'immutable';
-import { store } from './redux/configureStore';
 import { Provider } from 'react-redux';
 import ReduxForm from './components/ReduxForm';
 import RadioButtons from './components/RadioButtons';
+import Highlight from './components/Highlight';
+import Grid from '@material-ui/core/Grid';
 import { Field } from 'redux-form/immutable';
 import Radio from '../src/Radio';
 import RadioField from '../src/RadioField';
 
+import { storiesOf } from '@storybook/react';
+import { fromJS } from 'immutable';
+import { store } from './redux/configureStore';
 import radioMarkdownText from './doc/radio.md';
 
 storiesOf('Radio', module)
@@ -23,25 +25,41 @@ storiesOf('Radio', module)
   .add(
     'with Field',
     () => {
-      const initialValues = fromJS({
-        RadioField: 'b'
-      });
-      return (
-        <ReduxForm initialValues={initialValues}>
-          <Field
-            name="RadioField"
-            component={RadioField}
-            label="radio with Field"
-            radioValue="a"
-          />
-          <Field
-            name="RadioField"
-            component={RadioField}
-            label="radio with Field checked"
-            radioValue="b"
-          />
-        </ReduxForm>
-      );
+      const Form = () => {
+        const [values, setValues] = React.useState({
+          gender: 'male'
+        });
+        const handleChange = values => {
+          setValues(values.toJS());
+        };
+        return (
+          <Grid container>
+            <Grid item xs={6}>
+              <ReduxForm onChange={handleChange} initialValues={fromJS(values)}>
+                <Field
+                  name="gender"
+                  component={RadioField}
+                  label="male"
+                  radioValue="male"
+                />
+                <Field
+                  name="gender"
+                  component={RadioField}
+                  label="female"
+                  radioValue="female"
+                />
+              </ReduxForm>
+            </Grid>
+            <Grid item xs={6}>
+              <Highlight
+                code={JSON.stringify(values, null, 4)}
+                type="language-json"
+              />
+            </Grid>
+          </Grid>
+        );
+      };
+      return <Form />;
     },
     {
       notes: radioMarkdownText,
