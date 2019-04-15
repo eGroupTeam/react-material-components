c67e36241095b88a73d6af3c1b78a2ffe1f8d690
SP_Penguin <abrcdf1023@gmail.com>
2019-04-15 15:07:03 +0800

    doc(CheckboxInputGroup) update form UI

--- stories/checkboxInputGroup.stories.jsx
+++ stories/checkboxInputGroup.stories.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
 import { fromJS } from 'immutable';
-import { store } from './redux/configureStore';
 import { Provider } from 'react-redux';
 import ReduxForm from './components/ReduxForm';
+import Highlight from './components/Highlight';
+import Grid from '@material-ui/core/Grid';
 import { Field } from 'redux-form/immutable';
-
-import checkboxInputGroupText from './doc/checkboxInputGroup.md';
 import CheckboxInputGroup from '../src/CheckboxInputGroup';
 import CheckboxInputGroupField from '../src/CheckboxInputGroupField';
+
+import { store } from './redux/configureStore';
+import { storiesOf } from '@storybook/react';
+import checkboxInputGroupText from './doc/checkboxInputGroup.md';
 
 storiesOf('CheckboxInputGroup', module)
   .addDecorator(story => <Provider store={store}>{story()}</Provider>)
@@ -98,45 +100,61 @@ storiesOf('CheckboxInputGroup', module)
   .add(
     'with Field',
     () => {
-      const initialValues = fromJS({
-        CheckboxInputGroupField: {
-          checkbox2: {
-            checked: true,
-            text: 'awesome!'
+      const Form = () => {
+        const [values, setValues] = React.useState({
+          field1: {
+            checkbox2: {
+              checked: true,
+              text: 'awesome!'
+            }
           }
-        }
-      });
-      return (
-        <ReduxForm initialValues={initialValues}>
-          <Field
-            name="CheckboxInputGroupField"
-            label="with Field"
-            component={CheckboxInputGroupField}
-            options={[
-              {
-                name: 'checkbox1',
-                label: 'normal checkbox',
-                MuiCheckboxProps: {
-                  color: 'primary'
-                }
-              },
-              {
-                name: 'checkbox2',
-                label: 'checked with text input',
-                MuiCheckboxProps: {
-                  color: 'primary'
-                },
-                toggleInput: true
-              },
-              {
-                name: 'checkbox3',
-                label: 'checked with text input',
-                toggleInput: true
-              }
-            ]}
-          />
-        </ReduxForm>
-      );
+        });
+        const handleChange = values => {
+          setValues(values.toJS());
+        };
+        return (
+          <Grid container>
+            <Grid item xs={6}>
+              <ReduxForm onChange={handleChange} initialValues={fromJS(values)}>
+                <Field
+                  name="field1"
+                  label="with Field"
+                  component={CheckboxInputGroupField}
+                  options={[
+                    {
+                      name: 'checkbox1',
+                      label: 'normal checkbox',
+                      MuiCheckboxProps: {
+                        color: 'primary'
+                      }
+                    },
+                    {
+                      name: 'checkbox2',
+                      label: 'checked with text input',
+                      MuiCheckboxProps: {
+                        color: 'primary'
+                      },
+                      toggleInput: true
+                    },
+                    {
+                      name: 'checkbox3',
+                      label: 'checked with text input',
+                      toggleInput: true
+                    }
+                  ]}
+                />
+              </ReduxForm>
+            </Grid>
+            <Grid item xs={6}>
+              <Highlight
+                code={JSON.stringify(values, null, 4)}
+                type="language-json"
+              />
+            </Grid>
+          </Grid>
+        );
+      };
+      return <Form />;
     },
     {
       notes: checkboxInputGroupText,
