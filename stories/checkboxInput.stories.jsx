4faea77e17627026f6ad9d93cac862ce4eea9459
SP_Penguin <abrcdf1023@gmail.com>
2019-04-15 15:16:54 +0800

    doc(CheckboxInput) update form UI

--- stories/checkboxInput.stories.jsx
+++ stories/checkboxInput.stories.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { action } from '@storybook/addon-actions';
-import { storiesOf } from '@storybook/react';
-import { fromJS } from 'immutable';
-import { store } from './redux/configureStore';
 import { Provider } from 'react-redux';
 import ReduxForm from './components/ReduxForm';
+import Highlight from './components/Highlight';
+import Grid from '@material-ui/core/Grid';
 import { Field } from 'redux-form/immutable';
-
-import checkboxInputMarkdownText from './doc/checkboxInput.md';
 import CheckboxInput, { CheckboxInputComponent } from '../src/CheckboxInput';
 import CheckboxInputField from '../src/CheckboxInputField';
+
+import { fromJS } from 'immutable';
+import { storiesOf } from '@storybook/react';
+import { store } from './redux/configureStore';
+import checkboxInputMarkdownText from './doc/checkboxInput.md';
 
 storiesOf('CheckboxInput', module)
   .addDecorator(story => <Provider store={store}>{story()}</Provider>)
@@ -17,10 +18,6 @@ storiesOf('CheckboxInput', module)
     'default',
     () => (
       <CheckboxInput
-        MuiCheckboxProps={{
-          variant: 'contained',
-          onClick: action('clicked!')
-        }}
         MuiInputProps={{
           value: 'awesome!'
         }}
@@ -43,23 +40,17 @@ storiesOf('CheckboxInput', module)
       const Controled = () => {
         const [checked, setChecked] = React.useState(true);
         return (
-          <React.Fragment>
-            <CheckboxInput
-              MuiCheckboxProps={{
-                variant: 'contained'
-              }}
-              MuiInputProps={{
-                value: 'awesome!'
-              }}
-              checked={checked}
-              onChange={e => {
-                console.log('checked!');
-                setChecked(!checked);
-              }}
-              toggleInput
-              label="with controled checked"
-            />
-          </React.Fragment>
+          <CheckboxInput
+            MuiInputProps={{
+              value: 'awesome!'
+            }}
+            checked={checked}
+            onChange={e => {
+              setChecked(!checked);
+            }}
+            toggleInput
+            label="with controled checked"
+          />
         );
       };
       return <Controled />;
@@ -75,27 +66,43 @@ storiesOf('CheckboxInput', module)
   .add(
     'with Field',
     () => {
-      const initialValues = fromJS({
-        CheckboxInputField: {
-          checked: true,
-          text: 'awesome!'
-        }
-      });
-      return (
-        <ReduxForm initialValues={initialValues}>
-          <Field
-            name="CheckboxInputField"
-            component={CheckboxInputField}
-            MuiCheckboxProps={{
-              variant: 'contained',
-              onClick: action('clicked!')
-            }}
-            defaultChecked
-            toggleInput
-            label="with Field"
-          />
-        </ReduxForm>
-      );
+      const Form = () => {
+        const [values, setValues] = React.useState({
+          field1: {
+            checked: true,
+            text: 'awesome!'
+          }
+        });
+        const handleChange = values => {
+          setValues(values.toJS());
+        };
+        return (
+          <Grid container>
+            <Grid item xs={6}>
+              <ReduxForm onChange={handleChange} initialValues={fromJS(values)}>
+                <Field
+                  name="field1"
+                  component={CheckboxInputField}
+                  toggleInput
+                  label="with Field"
+                />
+                <Field
+                  name="field2"
+                  component={CheckboxInputField}
+                  label="with Field"
+                />
+              </ReduxForm>
+            </Grid>
+            <Grid item xs={6}>
+              <Highlight
+                code={JSON.stringify(values, null, 4)}
+                type="language-json"
+              />
+            </Grid>
+          </Grid>
+        );
+      };
+      return <Form />;
     },
     {
       notes: checkboxInputMarkdownText,
