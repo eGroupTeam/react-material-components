bd891ef60f0e6e6d4d41d64ea0c780909652f857
SP_Penguin <abrcdf1023@gmail.com>
2019-04-25 14:45:15 +0800

    [FormControlEditorField] new component done

--- src/lab/index.js
+++ src/lab/index.js
@@ -3,3 +3,4 @@ export { default as ButtonMenu } from './ButtonMenu';
 export { default as DataList } from './DataList';
 export { default as DatePickerField } from './DatePickerField';
 export { default as FormControlEditor } from './FormControlEditor';
+export { default as FormControlEditorField } from './FormControlEditorField';
