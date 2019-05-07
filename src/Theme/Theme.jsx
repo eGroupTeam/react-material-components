0949db9d614ed40bf2a4ae847f70cf1b7085773a
SP_Penguin <abrcdf1023@gmail.com>
2019-05-07 12:19:48 +0800

    [Theme] minor change

--- src/Theme/Theme.jsx
+++ src/Theme/Theme.jsx
@@ -3,12 +3,15 @@ import PropTypes from 'prop-types';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
-const Theme = ({ theme, children }) => (
-  <MuiThemeProvider theme={theme}>
-    <CssBaseline />
-    {children}
-  </MuiThemeProvider>
-);
+const Theme = ({ theme, children }) => {
+  if (process.env.NODE_ENV !== 'production') console.log(theme);
+  return (
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </MuiThemeProvider>
+  );
+};
 
 Theme.propTypes = {
   theme: PropTypes.object.isRequired,
