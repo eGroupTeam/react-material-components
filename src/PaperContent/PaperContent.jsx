ecebe1f56236e63334a1260c6a87ac0b0e7e22c8
SP_Penguin <abrcdf1023@gmail.com>
2019-05-07 16:31:12 +0800

    [PaperContent] feat add unit props

--- src/PaperContent/PaperContent.jsx
+++ src/PaperContent/PaperContent.jsx
@@ -3,23 +3,43 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import withStyles from '@material-ui/core/styles/withStyles';
 
-import styles from './styles';
+const styles = theme => ({
+  unit3: {
+    padding: `${theme.spacing.unit * 3}px`
+  },
+  unit2: {
+    padding: `${theme.spacing.unit * 2}px`
+  },
+  unit1: {
+    padding: `${theme.spacing.unit * 1}px`
+  }
+});
 
-const PaperContent = ({ classes, className, children, ...rest }) => (
-  <div className={classNames(classes.root, className)} {...rest}>
-    {children}
-  </div>
+const PaperContent = ({ classes, className, unit, ...other }) => (
+  <div className={classNames(classes[`unit${unit}`], className)} {...other} />
 );
 
 PaperContent.propTypes = {
-  // react-material props
+  /**
+   * Override or extend the styles applied to the component.
+   */
   classes: PropTypes.object.isRequired,
-  // customized props
+  /**
+   * JSX attribute
+   */
   className: PropTypes.node,
   /**
    * The content of the component.
    */
-  children: PropTypes.node
+  children: PropTypes.node,
+  /**
+   * The number of PaperContent padding unit.
+   */
+  unit: PropTypes.number
+};
+
+PaperContent.defaultProps = {
+  unit: 3
 };
 
 export default withStyles(styles)(PaperContent);
