f34d70cff6014124851b9a376137796c9da94323
SP_Penguin <abrcdf1023@gmail.com>
2019-05-22 16:00:11 +0800

    [AutoComplete] minor change

--- src/AutoComplete/AutoComplete.jsx
+++ src/AutoComplete/AutoComplete.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import { emphasize } from '@material-ui/core/styles/colorManipulator';
 import Select, { components } from 'react-select';
 import withStyles from '@material-ui/core/styles/withStyles';
 import Typography from '@material-ui/core/Typography';
@@ -10,7 +11,56 @@ import Chip from '@material-ui/core/Chip';
 import MenuItem from '@material-ui/core/MenuItem';
 import CancelIcon from '@material-ui/icons/Cancel';
 
-import styles from './styles';
+const styles = theme => ({
+  input: {
+    display: 'flex'
+  },
+  singleInput: {
+    height: '21px'
+  },
+  valueContainer: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    flex: 1,
+    alignItems: 'center',
+    overflow: 'hidden'
+  },
+  chip: {
+    margin: `${theme.spacing.unit / 2}px ${theme.spacing.unit / 4}px`
+  },
+  chipFocused: {
+    backgroundColor: emphasize(
+      theme.palette.type === 'light'
+        ? theme.palette.grey[300]
+        : theme.palette.grey[700],
+      0.08
+    )
+  },
+  noOptionsMessage: {
+    padding: `${theme.spacing.unit}px ${theme.spacing.unit * 2}px`
+  },
+  singleValue: {
+    fontSize: 16
+  },
+  placeholder: {
+    position: 'absolute',
+    left: 2,
+    fontSize: 16
+  },
+  paper: {
+    position: 'absolute',
+    zIndex: 1,
+    marginTop: theme.spacing.unit,
+    left: 0,
+    right: 0
+  },
+  divider: {
+    height: theme.spacing.unit * 2
+  },
+  indicator: {
+    cursor: 'pointer'
+  }
+});
 
 function NoOptionsMessage(props) {
   return (
