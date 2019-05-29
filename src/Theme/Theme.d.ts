047d28d9dcfc03b1721d6e16869b0d2186b84c95
SP_Penguin <abrcdf1023@gmail.com>
2019-05-29 17:19:05 +0800

    [types] add Theme

---
+++ src/Theme/Theme.d.ts
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { ThemeProviderProps } from '@material-ui/styles/ThemeProvider';
+
+declare const Theme: React.ComponentType<ThemeProviderProps>;
+
+export default Theme;
--- src/Theme/Theme.jsx
+++ src/Theme/Theme.jsx
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
-const Theme = ({ theme, children }) => {
+const Theme = ({ theme, children, ...other }) => {
   if (process.env.NODE_ENV !== 'production') console.log(theme);
   return (
-    <MuiThemeProvider theme={theme}>
+    <MuiThemeProvider theme={theme} {...other}>
       <CssBaseline />
       {children}
     </MuiThemeProvider>
