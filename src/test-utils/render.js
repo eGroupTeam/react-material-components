a876a4b411fda490a53ee20179acc72fe6514aaa
SP_Penguin <abrcdf1023@gmail.com>
2019-06-04 11:06:36 +0800

    [test] add a util to use static JSS classname https://github.com/mui-org/material-ui/issues/9492

---
+++ src/test-utils/render.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render as testingRender } from '@testing-library/react';
+import { StylesProvider } from '@material-ui/styles';
+
+const generateClassName = (rule, styleSheet) =>
+  `${styleSheet.options.classNamePrefix}-${rule.key}`;
+
+export default function render(children) {
+  return testingRender(
+    <StylesProvider generateClassName={generateClassName}>
+      {children}
+    </StylesProvider>
+  );
+}
