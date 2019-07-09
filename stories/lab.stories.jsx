94dfdbc25da7f4e089f3605c454ac629c123c4c5
SP_Penguin <abrcdf1023@gmail.com>
2019-07-09 12:44:27 +0800

    [lab] add SlateEditor

--- stories/lab.stories.jsx
+++ stories/lab.stories.jsx
@@ -24,6 +24,8 @@ import DataList from '@e-group/material-lab/DataList';
 import DatePickerField from '@e-group/material-lab/DatePickerField';
 import FormControlEditor from '@e-group/material-lab/FormControlEditor';
 import FormControlEditorField from '@e-group/material-lab/FormControlEditorField';
+import SlateEditor from '@e-group/material-lab/SlateEditor';
+import { Value } from 'slate'
 
 storiesOf('Lab', module)
   .addDecorator(story => <Provider store={store}>{story()}</Provider>)
@@ -397,6 +399,91 @@ storiesOf('Lab', module)
     {
       info: {
         propTables: [FormControlEditor]
+      }
+    }
+  )
+  .add(
+    'SlateEditor',
+    () => {
+      const initialValue = Value.fromJSON({
+        document: {
+          nodes: [
+            {
+              object: 'block',
+              type: 'paragraph',
+              nodes: [
+                {
+                  object: 'text',
+                  text: 'A line of text in a paragraph.',
+                },
+              ],
+            },
+          ],
+        },
+      })
+      function MarkHotkey(options) {
+        // Grab our options from the ones passed in.
+        const { type, key } = options
+
+        // Return our "plugin" object, containing the `onKeyDown` handler.
+        return {
+          onKeyDown(event, editor, next) {
+            // If it doesn't match our `key`, let other plugins handle it.
+            if (!event.ctrlKey || event.key != key) return next()
+
+            // Prevent the default characters from being inserted.
+            event.preventDefault()
+
+            // Toggle the mark `type`.
+            editor.toggleMark(type)
+          },
+        }
+      }
+      const plugins = [
+        MarkHotkey({ key: 'b', type: 'bold' }),
+        MarkHotkey({ key: '`', type: 'code' }),
+        MarkHotkey({ key: 'i', type: 'italic' }),
+        MarkHotkey({ key: '~', type: 'strikethrough' }),
+        MarkHotkey({ key: 'u', type: 'underline' }),
+      ]
+      const Demo = () => {
+        const [value, setValue] = React.useState(initialValue)
+
+        const onChange = ({ value }) => {
+          setValue(value)
+        }
+        // Add a `renderMark` method to render marks.
+        const renderMark = (props, editor, next) => {
+          switch (props.mark.type) {
+            case 'bold':
+              return <strong>{props.children}</strong>
+            // Add our new mark renderers...
+            case 'code':
+              return <code>{props.children}</code>
+            case 'italic':
+              return <em>{props.children}</em>
+            case 'strikethrough':
+              return <del>{props.children}</del>
+            case 'underline':
+              return <u>{props.children}</u>
+            default:
+              return next()
+          }
+        }
+        return (
+          <SlateEditor
+            plugins={plugins}
+            value={value}
+            onChange={onChange}
+            renderMark={renderMark}
+          />
+        )
+      };
+      return <Demo />;
+    },
+    {
+      info: {
+        propTables: [ButtonMenu]
       }
     }
   );
