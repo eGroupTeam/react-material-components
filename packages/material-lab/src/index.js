0a9aa99abe0f07c21d9bc399620dd0c82ba16a61
SP_Penguin <abrcdf1023@gmail.com>
2019-08-01 14:37:07 +0800

    [material] move new feature to lab

--- packages/material-lab/src/index.js
+++ packages/material-lab/src/index.js
@@ -1,3 +1,5 @@
 export { default as ButtonMenu } from './ButtonMenu';
 export { default as FormControlEditor } from './FormControlEditor';
 export { default as FormControlEditorField } from './FormControlEditorField';
+export { default as ImageAutosize } from './ImageAutosize';
+export { default as Video } from './Video';
