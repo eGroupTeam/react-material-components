2b70ed3748c40bf4ca49cbb0528c26141f67073b
SP_Penguin <abrcdf1023@gmail.com>
2019-08-12 14:01:48 +0800

    [story] optimize AutoComplete user case

--- stories/autoComplete.stories.jsx
+++ stories/autoComplete.stories.jsx
@@ -4,7 +4,45 @@ import { storiesOf } from '@storybook/react';
 import autoCompleteMarkdownText from './doc/autoComplete.md';
 
 import AutoComplete from '@e-group/material-module/AutoComplete';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Typography from '@material-ui/core/Typography';
 import { boolean } from '@storybook/addon-knobs';
+
+const options = [{
+  userName: 'userName',
+  userOrganizationName: 'userOrganizationName',
+  userPhone: 'userPhone',
+  label: 'userName'
+}]
+
+const Option = (props) => {
+  const { userName, userPhone, userOrganizationName } = props.data;
+  return (
+    <ListItem
+      buttonRef={props.innerRef}
+      selected={props.isFocused}
+      button
+      style={{
+        fontWeight: props.isSelected ? 500 : 400
+      }}
+      {...props.innerProps}
+    >
+      <ListItemText
+        primary={userName}
+        secondary={
+          <React.Fragment>
+            <Typography component="span" color="textPrimary">
+              {userOrganizationName}
+            </Typography>
+            {' '}
+            {userPhone}
+          </React.Fragment>
+        }
+      />
+    </ListItem>
+  );
+}
 
 storiesOf('AutoComplete', module)
   .add(
@@ -16,6 +54,10 @@ storiesOf('AutoComplete', module)
           InputProps: {
             disableUnderline: boolean('DisableUnderline', false)
           }
+        }}
+        options={options}
+        components={{
+          Option
         }}
         placeholder="Search"
       />
