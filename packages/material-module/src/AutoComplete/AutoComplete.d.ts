47e5a94f14ad0ceb10012987f81077bbc9534c69
SP_Penguin <abrcdf1023@gmail.com>
2019-09-02 18:38:24 +0800

    [module] AutoComplete fixed types

--- packages/material-module/src/AutoComplete/AutoComplete.d.ts
+++ packages/material-module/src/AutoComplete/AutoComplete.d.ts
@@ -4,13 +4,9 @@ import Select from 'react-select/base';
 
 export interface AutoCompleteProps extends Select {
   /**
-   * Override or extend the styles applied to the component.
-   */
-  classes: object;
-  /**
    * Mui `TextField` props.
    */
-  MuiTextFieldProps: TextFieldProps;
+  MuiTextFieldProps?: TextFieldProps;
 }
 
 declare const AutoComplete: React.ComponentType<AutoCompleteProps>;
