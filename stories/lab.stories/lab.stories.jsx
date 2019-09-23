ad4f60cc8426dded3f752a97441d87473c5423dc
SP_Penguin <abrcdf1023@gmail.com>
2019-09-23 14:11:49 +0800

    [lab] basic SlateEditor done

--- stories/lab.stories/lab.stories.jsx
+++ stories/lab.stories/lab.stories.jsx
@@ -15,8 +15,10 @@ import { Provider } from 'react-redux';
 import ButtonMenu from '@e-group/material-lab/ButtonMenu';
 import FormControlEditor from '@e-group/material-lab/FormControlEditor';
 import FormControlEditorField from '@e-group/material-lab/FormControlEditorField';
+
 import SlateEditor from '@e-group/material-lab/SlateEditor';
 import { Value } from 'slate'
+
 import MediaStreamClipper from '@e-group/material-lab/MediaStreamClipper';
 import useGetVideoSnapshot from '@e-group/material-lab/MediaStreamClipper/useGetVideoSnapshot';
 import Snackbar from '@e-group/material-lab/Snackbar';
@@ -179,65 +181,7 @@ storiesOf('Lab', module)
           ],
         },
       })
-      function MarkHotkey(options) {
-        // Grab our options from the ones passed in.
-        const { type, key } = options
-
-        // Return our "plugin" object, containing the `onKeyDown` handler.
-        return {
-          onKeyDown(event, editor, next) {
-            // If it doesn't match our `key`, let other plugins handle it.
-            if (!event.ctrlKey || event.key != key) return next()
-
-            // Prevent the default characters from being inserted.
-            event.preventDefault()
-
-            // Toggle the mark `type`.
-            editor.toggleMark(type)
-          },
-        }
-      }
-      const plugins = [
-        MarkHotkey({ key: 'b', type: 'bold' }),
-        MarkHotkey({ key: '`', type: 'code' }),
-        MarkHotkey({ key: 'i', type: 'italic' }),
-        MarkHotkey({ key: '~', type: 'strikethrough' }),
-        MarkHotkey({ key: 'u', type: 'underline' }),
-      ]
-      const Demo = () => {
-        const [value, setValue] = React.useState(initialValue)
-
-        const onChange = ({ value }) => {
-          setValue(value)
-        }
-        // Add a `renderMark` method to render marks.
-        const renderMark = (props, editor, next) => {
-          switch (props.mark.type) {
-            case 'bold':
-              return <strong>{props.children}</strong>
-            // Add our new mark renderers...
-            case 'code':
-              return <code>{props.children}</code>
-            case 'italic':
-              return <em>{props.children}</em>
-            case 'strikethrough':
-              return <del>{props.children}</del>
-            case 'underline':
-              return <u>{props.children}</u>
-            default:
-              return next()
-          }
-        }
-        return (
-          <SlateEditor
-            plugins={plugins}
-            value={value}
-            onChange={onChange}
-            renderMark={renderMark}
-          />
-        )
-      };
-      return <Demo />;
+      return <SlateEditor />;
     },
     {
       info: {
