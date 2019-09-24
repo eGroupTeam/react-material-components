c10ab4004c6a9c3664d7147d42f053c8aa87349b
SP_Penguin <abrcdf1023@gmail.com>
2019-09-24 17:48:46 +0800

    [lab] add SlateEditor feature

--- packages/material-lab/src/SlateEditor/SlateEditor.jsx
+++ packages/material-lab/src/SlateEditor/SlateEditor.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
+import Html from 'slate-html-serializer';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import { isKeyHotkey } from 'is-hotkey';
 import clsx from 'clsx';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 
-import { Editor } from 'slate-react';
+import { Editor, getEventTransfer } from 'slate-react';
 import { Value } from 'slate';
 import ToggleButton from '@material-ui/lab/ToggleButton';
 import Paper from '@material-ui/core/Paper';
@@ -20,13 +21,6 @@ import FormatQuoteIcon from '@material-ui/icons/FormatQuote';
 import FormatListBulletedIcon from '@material-ui/icons/FormatListBulleted';
 import FormatListNumberedIcon from '@material-ui/icons/FormatListNumbered';
 import StyledToggleButtonGroup from './StyledToggleButtonGroup';
-
-const DEFAULT_NODE = 'paragraph';
-
-const isBoldHotkey = isKeyHotkey('mod+b');
-const isItalicHotkey = isKeyHotkey('mod+i');
-const isUnderlinedHotkey = isKeyHotkey('mod+u');
-const isCodeHotkey = isKeyHotkey('mod+`');
 
 const useStyles = makeStyles(theme => ({
   toolbar: {
@@ -55,8 +49,171 @@ const useStyles = makeStyles(theme => ({
     padding: `10px ${theme.spacing(2)}px`,
     textAlign: 'right',
     border: `1px solid ${theme.palette.divider}`
+  },
+  img: {
+    display: 'block',
+    maxWidth: '100%',
+    maxHeight: '20em'
+    // boxShadow: `${isFocused ? '0 0 0 2px blue;' : 'none'}`
   }
 }));
+
+/**
+ * Define the default node type.
+ *
+ * @type {String}
+ */
+const DEFAULT_NODE = 'paragraph';
+
+/**
+ * Define hotkey matchers.
+ *
+ * @type {Function}
+ */
+const isBoldHotkey = isKeyHotkey('mod+b');
+const isItalicHotkey = isKeyHotkey('mod+i');
+const isUnderlinedHotkey = isKeyHotkey('mod+u');
+const isCodeHotkey = isKeyHotkey('mod+`');
+
+/**
+ * Tags to blocks.
+ *
+ * @type {Object}
+ */
+
+const BLOCK_TAGS = {
+  p: 'paragraph',
+  li: 'list-item',
+  ul: 'bulleted-list',
+  ol: 'numbered-list',
+  blockquote: 'quote',
+  pre: 'code',
+  h1: 'heading-one',
+  h2: 'heading-two',
+  h3: 'heading-three',
+  h4: 'heading-four',
+  h5: 'heading-five',
+  h6: 'heading-six'
+};
+
+/**
+ * Tags to marks.
+ *
+ * @type {Object}
+ */
+
+const MARK_TAGS = {
+  strong: 'bold',
+  em: 'italic',
+  u: 'underline',
+  s: 'strikethrough',
+  code: 'code'
+};
+
+/**
+ * Serializer rules.
+ *
+ * @type {Array}
+ */
+
+const RULES = [
+  {
+    deserialize(el, next) {
+      const block = BLOCK_TAGS[el.tagName.toLowerCase()];
+
+      if (block) {
+        return {
+          object: 'block',
+          type: block,
+          nodes: next(el.childNodes)
+        };
+      }
+    }
+  },
+  {
+    deserialize(el, next) {
+      const mark = MARK_TAGS[el.tagName.toLowerCase()];
+
+      if (mark) {
+        return {
+          object: 'mark',
+          type: mark,
+          nodes: next(el.childNodes)
+        };
+      }
+    }
+  },
+  {
+    // Special case for code blocks, which need to grab the nested childNodes.
+    deserialize(el, next) {
+      if (el.tagName.toLowerCase() === 'pre') {
+        const code = el.childNodes[0];
+        const childNodes =
+          code && code.tagName.toLowerCase() === 'code'
+            ? code.childNodes
+            : el.childNodes;
+
+        return {
+          object: 'block',
+          type: 'code',
+          nodes: next(childNodes)
+        };
+      }
+    }
+  },
+  {
+    // Special case for images, to grab their src.
+    deserialize(el, next) {
+      if (el.tagName.toLowerCase() === 'img') {
+        return {
+          object: 'block',
+          type: 'image',
+          nodes: next(el.childNodes),
+          data: {
+            src: el.getAttribute('src')
+          }
+        };
+      }
+    }
+  },
+  {
+    // Special case for links, to grab their href.
+    deserialize(el, next) {
+      if (el.tagName.toLowerCase() === 'a') {
+        return {
+          object: 'inline',
+          type: 'link',
+          nodes: next(el.childNodes),
+          data: {
+            href: el.getAttribute('href')
+          }
+        };
+      }
+    }
+  }
+];
+
+/**
+ * Create a new HTML serializer with `RULES`.
+ *
+ * @type {Html}
+ */
+
+const serializer = new Html({ rules: RULES });
+
+/**
+ * The editor's schema.
+ *
+ * @type {Object}
+ */
+
+const schema = {
+  blocks: {
+    image: {
+      isVoid: true
+    }
+  }
+};
 
 const SlateEditor = React.forwardRef(function SlateEditor(props, ref) {
   const { initialValues, ...other } = props;
@@ -73,6 +230,12 @@ const SlateEditor = React.forwardRef(function SlateEditor(props, ref) {
   const { document, activeMarks, blocks } = value;
   const activeMarkTypes = activeMarks.map(el => el.get('type'));
   let blockTypes = blocks.map(el => el.get('type'));
+  if (blocks.size > 0) {
+    const parent = document.getParent(blocks.first().key);
+    if (blockTypes.includes('list-item') && parent) {
+      blockTypes = blockTypes.push(parent.type);
+    }
+  }
 
   React.useEffect(() => {
     const handleResize = () => setWidth(rootEl.current.offsetWidth);
@@ -84,13 +247,6 @@ const SlateEditor = React.forwardRef(function SlateEditor(props, ref) {
       window.removeEventListener('resize', handleResize);
     };
   }, []);
-
-  if (blocks.size > 0) {
-    const parent = document.getParent(blocks.first().key);
-    if (blockTypes.includes('list-item') && parent) {
-      blockTypes = blockTypes.push(parent.type);
-    }
-  }
 
   const renderMarkButton = (type, icon) => {
     return (
@@ -118,18 +274,40 @@ const SlateEditor = React.forwardRef(function SlateEditor(props, ref) {
     const { attributes, children, node } = props;
 
     switch (node.type) {
+      case 'paragraph':
+        return <p {...attributes}>{children}</p>;
       case 'block-quote':
         return <blockquote {...attributes}>{children}</blockquote>;
+      case 'code':
+        return (
+          <pre>
+            <code {...attributes}>{children}</code>
+          </pre>
+        );
       case 'bulleted-list':
         return <ul {...attributes}>{children}</ul>;
       case 'heading-one':
         return <h1 {...attributes}>{children}</h1>;
       case 'heading-two':
         return <h2 {...attributes}>{children}</h2>;
+      case 'heading-three':
+        return <h3 {...attributes}>{children}</h3>;
+      case 'heading-four':
+        return <h4 {...attributes}>{children}</h4>;
+      case 'heading-five':
+        return <h5 {...attributes}>{children}</h5>;
+      case 'heading-six':
+        return <h6 {...attributes}>{children}</h6>;
       case 'list-item':
         return <li {...attributes}>{children}</li>;
       case 'numbered-list':
         return <ol {...attributes}>{children}</ol>;
+      case 'image':
+        const src = node.data.get('src');
+        return (
+          // eslint-disable-next-line jsx-a11y/alt-text
+          <img {...attributes} src={src} className={classes.img} />
+        );
       default:
         return next();
     }
@@ -149,6 +327,55 @@ const SlateEditor = React.forwardRef(function SlateEditor(props, ref) {
         return <u {...attributes}>{children}</u>;
       default:
         return next();
+    }
+  };
+
+  /**
+   * Render a Slate inline.
+   *
+   * @param {Object} props
+   * @param {Editor} editor
+   * @param {Function} next
+   * @return {Element}
+   */
+
+  const renderInline = (props, editor, next) => {
+    const { attributes, children, node } = props;
+
+    switch (node.type) {
+      case 'link': {
+        const { data } = node;
+        const href = data.get('href');
+        return (
+          <a {...attributes} href={href}>
+            {children}
+          </a>
+        );
+      }
+
+      default: {
+        return next();
+      }
+    }
+  };
+
+  /**
+   * On paste, deserialize the HTML and then insert the fragment.
+   *
+   * @param {Event} event
+   * @param {Editor} editor
+   */
+
+  const onPaste = (event, editor, next) => {
+    event.preventDefault();
+
+    const transfer = getEventTransfer(event);
+    const { type, html } = transfer;
+    if (type === 'html') {
+      const { document } = serializer.deserialize(html);
+      editor.insertFragment(document);
+    } else {
+      return next();
     }
   };
 
@@ -252,10 +479,13 @@ const SlateEditor = React.forwardRef(function SlateEditor(props, ref) {
           <Editor
             ref={editorEl}
             value={value}
+            schema={schema}
             onChange={onChange}
             onKeyDown={onKeyDown}
+            onPaste={onPaste}
             renderBlock={renderBlock}
             renderMark={renderMark}
+            renderInline={renderInline}
             {...other}
           />
         </div>
