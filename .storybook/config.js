14d83ee617973329be40d7edb756a130f84d324f
SP_Penguin <abrcdf1023@gmail.com>
2019-10-04 14:14:40 +0800

    update storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,14 @@
-import { configure, addDecorator } from '@storybook/react';
+import { configure, addDecorator, addParameters } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 import { withNotes } from '@storybook/addon-notes';
 import { withKnobs } from '@storybook/addon-knobs';
+import theme from './theme';
 
+addParameters({
+  options: {
+    theme
+  },
+});
 addDecorator(withKnobs)
 addDecorator(withInfo);
 addDecorator(withNotes);
---
+++ .storybook/theme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+
+  brandTitle: 'eGroup Material',
+  brandUrl: 'https://abrcdf1023.github.io/egroup-material',
+});
