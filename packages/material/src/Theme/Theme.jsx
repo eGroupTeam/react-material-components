5166d78844102aa641cd5a5daeaf9c8c40324800
SP_Penguin <abrcdf1023@gmail.com>
2019-10-22 17:07:47 +0800

    [chore] update dependencies versions

--- packages/material/src/Theme/Theme.jsx
+++ packages/material/src/Theme/Theme.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 const Theme = ({ theme, children, ...other }) => {
   if (process.env.NODE_ENV !== 'production') console.log(theme);
   return (
-    <MuiThemeProvider theme={theme} {...other}>
+    <ThemeProvider theme={theme} {...other}>
       <CssBaseline />
       {children}
-    </MuiThemeProvider>
+    </ThemeProvider>
   );
 };
 
