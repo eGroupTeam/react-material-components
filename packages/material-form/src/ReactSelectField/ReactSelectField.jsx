747295996d7baa08cdba0dd03a695522c5386da9
SP_Penguin <abrcdf1023@gmail.com>
2019-12-13 18:39:01 +0800

    [form] ReactSelectField use hook

--- packages/material-form/src/ReactSelectField/ReactSelectField.jsx
+++ packages/material-form/src/ReactSelectField/ReactSelectField.jsx
@@ -1,29 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import ReactSelect from '@e-group/material-module/ReactSelect';
 import { isImmutable, fromJS } from 'immutable';
 
-export default class ReactSelectField extends Component {
-  static propTypes = {
-    /**
-     * redux from props
-     */
-    input: PropTypes.object.isRequired,
-    meta: PropTypes.object.isRequired,
-    /** Callback function that triggers when the search text value has changed.
-     * function(option: object) => void */
-    onChange: PropTypes.func
-  };
-
-  state = {
-    inputValue: ''
-  };
-
-  handleChange = option => {
+const ReactSelectField = props => {
+  const {
+    input,
+    meta,
+    onChange,
+    onInputChange,
+    options,
+    inputValue: inputValueProp,
+    value,
+    ...other
+  } = props;
+  const [inputValue, setInputValue] = React.useState('');
+  const handleChange = option => {
     const value = isImmutable(option) ? option : fromJS(option);
-    const { onChange, input } = this.props;
     if (onChange) {
-      onChange(value, this.props);
+      onChange(value, props);
     } else {
       input.onChange(value);
     }
@@ -31,38 +26,36 @@ export default class ReactSelectField extends Component {
 
   // To keep value after onBlur please read this issue.
   // https://github.com/JedWatson/react-select/issues/3189
-  handleInputChange = (inputValue, action) => {
-    const { onInputChange } = this.props;
+  const handleInputChange = (inputValue, action) => {
     if (onInputChange) {
       onInputChange(inputValue, action);
     }
     if (action.action !== 'input-blur' && action.action !== 'menu-close') {
-      this.setState({ inputValue });
+      setInputValue(inputValue);
     }
   };
 
-  render() {
-    const { inputValue } = this.state;
-    const {
-      input,
-      meta,
-      onChange,
-      onInputChange,
-      options,
-      inputValue: inputValueProp,
-      value,
-      ...other
-    } = this.props;
+  return (
+    <ReactSelect
+      inputValue={inputValue}
+      onChange={handleChange}
+      onInputChange={handleInputChange}
+      options={options}
+      value={input.value && input.value.toJS()}
+      {...other}
+    />
+  );
+};
 
-    return (
-      <ReactSelect
-        inputValue={inputValue}
-        onChange={this.handleChange}
-        onInputChange={this.handleInputChange}
-        options={options}
-        value={input.value && input.value.toJS()}
-        {...other}
-      />
-    );
-  }
-}
+ReactSelectField.propTypes = {
+  /**
+   * redux from props
+   */
+  input: PropTypes.object.isRequired,
+  meta: PropTypes.object.isRequired,
+  /** Callback function that triggers when the search text value has changed.
+   * function(option: object) => void */
+  onChange: PropTypes.func
+};
+
+export default ReactSelectField;
