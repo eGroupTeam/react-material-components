1809e66ef81bf46e5abf78197f2fbb41667778b5
SP_Penguin <abrcdf1023@gmail.com>
2020-01-31 17:28:32 +0800

    [core] update CheckboxInputGroup to hook

--- packages/material/src/CheckboxInputGroup/CheckboxInputGroup.jsx
+++ packages/material/src/CheckboxInputGroup/CheckboxInputGroup.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import warning from 'warning';
 import FormControl from '@material-ui/core/FormControl';
@@ -7,65 +7,65 @@ import FormGroup from '@material-ui/core/FormGroup';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import CheckboxInput from '../CheckboxInput';
 
-export default class CheckboxInputGroup extends Component {
-  static propTypes = {
-    /**
-     * The content of the FormLabel.
-     */
-    label: PropTypes.string,
-    /**
-     * Options to generate group items.
-     */
-    options: PropTypes.array.isRequired,
-    /**
-     * The content of the FormHelperText.
-     */
-    helperText: PropTypes.string,
-    /**
-     * Mui `FormLabel` Props
-     */
-    MuiFormLabelProps: PropTypes.object,
-    /**
-     * Mui `FormGroup` Props
-     */
-    MuiFormGroupProps: PropTypes.object,
-    /**
-     * Mui `FormHelperText` Props
-     */
-    MuiFormHelperTextProps: PropTypes.object
-  };
+const CheckboxInputGroup = props => {
+  const {
+    label,
+    options,
+    helperText,
+    MuiFormLabelProps,
+    MuiFormGroupProps,
+    MuiFormHelperTextProps,
+    children,
+    ...other
+  } = props;
 
-  render() {
-    const {
-      label,
-      options,
-      helperText,
-      MuiFormLabelProps,
-      MuiFormGroupProps,
-      MuiFormHelperTextProps,
-      children,
-      ...other
-    } = this.props;
+  warning(
+    children === undefined,
+    'CheckboxInputGroup should not has children please use `options` only!'
+  );
 
-    warning(
-      children === undefined,
-      'CheckboxInputGroup should not has children please use `options` only!'
-    );
+  return (
+    <FormControl {...other}>
+      <FormLabel {...MuiFormLabelProps}>{label}</FormLabel>
+      <FormGroup {...MuiFormGroupProps}>
+        {options.map((option, index) => (
+          <CheckboxInput key={option.key || index} {...option} />
+        ))}
+      </FormGroup>
+      {helperText && (
+        <FormHelperText {...MuiFormHelperTextProps}>
+          {helperText}
+        </FormHelperText>
+      )}
+    </FormControl>
+  );
+};
 
-    return (
-      <FormControl {...other}>
-        <FormLabel {...MuiFormLabelProps}>{label}</FormLabel>
-        <FormGroup {...MuiFormGroupProps}>
-          {options.map((option, index) => (
-            <CheckboxInput key={option.key || index} {...option} />
-          ))}
-        </FormGroup>
-        {helperText && (
-          <FormHelperText {...MuiFormHelperTextProps}>
-            {helperText}
-          </FormHelperText>
-        )}
-      </FormControl>
-    );
-  }
-}
+CheckboxInputGroup.propTypes = {
+  /**
+   * The content of the FormLabel.
+   */
+  label: PropTypes.string,
+  /**
+   * Options to generate group items.
+   */
+  options: PropTypes.array.isRequired,
+  /**
+   * The content of the FormHelperText.
+   */
+  helperText: PropTypes.string,
+  /**
+   * Mui `FormLabel` Props
+   */
+  MuiFormLabelProps: PropTypes.object,
+  /**
+   * Mui `FormGroup` Props
+   */
+  MuiFormGroupProps: PropTypes.object,
+  /**
+   * Mui `FormHelperText` Props
+   */
+  MuiFormHelperTextProps: PropTypes.object
+};
+
+export default CheckboxInputGroup;
