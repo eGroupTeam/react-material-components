274edc6f0bd80cae8333478eb6d01abc26bfefe8
SP_Penguin <abrcdf1023@gmail.com>
2020-04-08 15:06:07 +0800

    [lab] some minor change

--- packages/material-lab/src/CKEditor/CKEditor.jsx
+++ packages/material-lab/src/CKEditor/CKEditor.jsx
@@ -1,14 +1,9 @@
-import React from 'react'
+import React from 'react';
 import SourceCKEditor from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@e-group/ckeditor5-build-classic';
 
 const CKEditor = props => {
-  return (
-    <SourceCKEditor
-      editor={ ClassicEditor }
-      {...props}
-    />
-  )
-}
+  return <SourceCKEditor editor={ClassicEditor} {...props} />;
+};
 
-export default CKEditor
+export default CKEditor;
