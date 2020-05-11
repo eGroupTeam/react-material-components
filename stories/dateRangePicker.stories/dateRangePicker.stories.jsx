349fe31b274d4a27f98b5a304da0ba4bf3e0d726
SP_Penguin <abrcdf1023@gmail.com>
2020-05-11 16:49:30 +0800

    [lab] feat basic DateRangePicker

--- stories/dateRangePicker.stories/dateRangePicker.stories.jsx
+++ stories/dateRangePicker.stories/dateRangePicker.stories.jsx
@@ -6,9 +6,19 @@ import DateRangePicker from '@e-group/material-lab/DateRangePicker';
 storiesOf('DateRangePicker', module)
   .add(
     'default',
-    () => (
-      <DateRangePicker />
-    ),
+    () => {
+      const [dateRange, setDateRange] = React.useState({})
+
+      const Demo = () => {
+        return (
+          <>
+            <DateRangePicker open onChange={range => setDateRange(range)}/>
+            {JSON.stringify(dateRange)}
+          </>
+        )
+      }
+      return <Demo />
+    },
     {
       info: {
         propTables: [DateRangePicker]
