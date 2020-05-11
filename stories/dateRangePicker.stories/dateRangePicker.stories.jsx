88ee399b4e422a89b5a7913e663c1c726dcb8704
SP_Penguin <abrcdf1023@gmail.com>
2020-05-11 18:13:42 +0800

    [story] dateRangePicker new example

--- stories/dateRangePicker.stories/dateRangePicker.stories.jsx
+++ stories/dateRangePicker.stories/dateRangePicker.stories.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import {
+  addDays,
+  startOfWeek,
+  endOfWeek,
+  addWeeks,
+  startOfMonth,
+  endOfMonth,
+  addMonths
+} from 'date-fns';
 
 import DateRangePicker from '@e-group/material-lab/DateRangePicker';
 
@@ -7,12 +16,53 @@ storiesOf('DateRangePicker', module)
   .add(
     'default',
     () => {
+      const date = new Date()
       const Demo = () => {
         const [dateRange, setDateRange] = React.useState({})
         
         return (
           <>
-            <DateRangePicker open onChange={range => setDateRange(range)}/>
+            <DateRangePicker
+              open
+              onChange={range => setDateRange(range)}
+              definedRanges={[
+                {
+                  label: '今天',
+                  startDate: date,
+                  endDate: date
+                },
+                // {
+                //   label: '昨天',
+                //   startDate: addDays(date, -1),
+                //   endDate: addDays(date, -1)
+                // },
+                {
+                  label: '本週',
+                  startDate: startOfWeek(date),
+                  endDate: endOfWeek(date)
+                },
+                // {
+                //   label: '上一週',
+                //   startDate: startOfWeek(addWeeks(date, -1)),
+                //   endDate: endOfWeek(addWeeks(date, -1))
+                // },
+                // {
+                //   label: '過去７天',
+                //   startDate: addWeeks(date, -1),
+                //   endDate: date
+                // },
+                {
+                  label: '這個月',
+                  startDate: startOfMonth(date),
+                  endDate: endOfMonth(date)
+                },
+                // {
+                //   label: '上個月',
+                //   startDate: startOfMonth(addMonths(date, -1)),
+                //   endDate: endOfMonth(addMonths(date, -1))
+                // }
+              ]}
+            />
             {JSON.stringify(dateRange)}
           </>
         )
