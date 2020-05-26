b6df0b47bc97553d09a98dc830ed753a7093e845
SP_Penguin <abrcdf1023@gmail.com>
2020-05-26 14:00:19 +0800

    [form module] remove  AutoComplete

--- packages/material-form/src/AutoCompleteField/AutoCompleteField.d.ts
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import { AutoCompleteProps } from '@e-group/material-module/AutoComplete';
-
-declare const AutoCompleteField: React.ComponentType<AutoCompleteProps>;
-
-export default AutoCompleteField;
