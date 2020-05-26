440219393368b729977d47ebf1978160391966a8
SP_Penguin <abrcdf1023@gmail.com>
2020-05-26 14:54:21 +0800

    [form] feat immutable CheckboxInputField

--- packages/material-form/src/CheckboxInputField/immutable/CheckboxInputField.jsx
+++ packages/material-form/src/CheckboxInputField/immutable/CheckboxInputField.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { fromJS, isImmutable } from 'immutable';
 
 import CheckboxInput from '@e-group/material/CheckboxInput';
 
@@ -14,34 +15,41 @@ const CheckboxInputField = ({
   MuiInputProps,
   ...other
 }) => {
+  const valueIsImmutable = isImmutable(value);
   const { onChange: inputOnChange, value: inputValue, ...otherMuiInputProps } =
     MuiInputProps || {};
 
   const handleChange = e => {
-    onChange({
-      ...value,
-      checked: e.target.checked
-    });
+    if (valueIsImmutable) {
+      onChange(value.set('checked', e.target.checked));
+    } else {
+      onChange(
+        fromJS({
+          checked: e.target.checked
+        })
+      );
+    }
   };
 
   const handleInputChange = e => {
-    onChange({
-      ...value,
-      text: e.target.value
-    });
+    if (valueIsImmutable) {
+      onChange(value.set('text', e.target.value));
+    } else {
+      onChange(
+        fromJS({
+          text: e.target.value
+        })
+      );
+    }
   };
-
-  const hasValue = typeof value !== 'undefined';
-  const hasChecked = hasValue && typeof value.checked !== 'undefined';
-  const hasText = hasValue && typeof value.text !== 'undefined';
 
   return (
     <CheckboxInput
       onChange={handleChange}
-      checked={hasChecked ? value.checked : false}
+      checked={valueIsImmutable ? value.get('checked', false) : false}
       MuiInputProps={{
         onChange: handleInputChange,
-        value: hasText ? value.text : '',
+        value: valueIsImmutable ? value.get('text', '') : '',
         ...otherMuiInputProps
       }}
       {...other}
--- stories/checkboxInput.stories/checkboxInput.stories.jsx
+++ stories/checkboxInput.stories/checkboxInput.stories.jsx
@@ -5,7 +5,7 @@ import Highlight from '../components/Highlight';
 import Grid from '@material-ui/core/Grid';
 import { Field } from 'redux-form/immutable';
 import CheckboxInput from '@e-group/material/CheckboxInput/CheckboxInput';
-import CheckboxInputField from '@e-group/material-form/CheckboxInputField';
+import CheckboxInputField from '@e-group/material-form/CheckboxInputField/immutable';
 
 import { fromJS } from 'immutable';
 import { storiesOf } from '@storybook/react';
@@ -57,7 +57,7 @@ storiesOf('CheckboxInput', module)
     }
   )
   .add(
-    'with Field',
+    'with immutableJS Field',
     () => {
       const Form = () => {
         const [values, setValues] = React.useState({
