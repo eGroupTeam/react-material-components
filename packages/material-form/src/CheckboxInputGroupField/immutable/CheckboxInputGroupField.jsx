63eedd6756adaedde623e92a768641146a9ad170
SP_Penguin <abrcdf1023@gmail.com>
2020-05-26 19:10:28 +0800

    [form] CheckboxInputGroupField immutable split

--- packages/material-form/src/CheckboxInputGroupField/immutable/CheckboxInputGroupField.jsx
+++ packages/material-form/src/CheckboxInputGroupField/immutable/CheckboxInputGroupField.jsx
@@ -1,55 +1,61 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { fromJS, isImmutable } from 'immutable';
 
 import CheckboxInputGroup from '@e-group/material/CheckboxInputGroup';
 
 // Code below is refer to https://github.com/erikras/redux-form/issues/1037
 const CheckboxInputGroupField = ({
-  input: { value, onChange },
+  input,
   meta: { touched, invalid, error },
   options,
   error: errorProp,
   helperText,
   ...other
 }) => {
+  const valueIsImmutable = isImmutable(input.value);
   const isError = touched && invalid;
 
   const handleChange = (e, name) => {
-    onChange({
-      ...value,
-      [name]: {
-        ...value[name],
-        checked: e.target.checked
-      }
-    });
+    if (valueIsImmutable) {
+      input.onChange(input.value.setIn([name, 'checked'], e.target.checked));
+    } else {
+      input.onChange(
+        fromJS({
+          [name]: {
+            checked: e.target.checked
+          }
+        })
+      );
+    }
   };
 
   const handleInputChange = (e, name) => {
-    onChange({
-      ...value,
-      [name]: {
-        ...value[name],
-        text: e.target.value
-      }
-    });
+    if (valueIsImmutable) {
+      input.onChange(input.value.setIn([name, 'text'], e.target.value));
+    } else {
+      input.onChange(
+        fromJS({
+          [name]: {
+            text: e.target.value
+          }
+        })
+      );
+    }
   };
-
-  const hasValue = typeof value !== 'undefined';
 
   const nextOptions = options.map(
     ({ onChange, checked, MuiInputProps, name, ...otherOption }) => {
-      const hasOptionValue = hasValue && value[name];
-      const hasChecked =
-        hasOptionValue && typeof value[name].checked !== 'undefined';
-      const hasText = hasOptionValue && typeof value[name].text !== 'undefined';
       return {
         name,
         onChange: e => handleChange(e, name),
-        checked: hasChecked ? value[name].checked : false,
+        checked: valueIsImmutable
+          ? input.value.getIn([name, 'checked'], false)
+          : false,
         MuiInputProps: {
           ...MuiInputProps,
           onChange: e => handleInputChange(e, name),
-          value: hasText ? value[name].text : ''
+          value: valueIsImmutable ? input.value.getIn([name, 'text'], '') : ''
         },
         ...otherOption
       };
--- stories/checkboxInputGroup.stories/checkboxInputGroup.stories.jsx
+++ stories/checkboxInputGroup.stories/checkboxInputGroup.stories.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import ReduxForm from '../components/ReduxForm';
 import ImmutableJsReduxForm from '../components/ImmutableJsReduxForm';
 import Highlight from '../components/Highlight';
 import Grid from '@material-ui/core/Grid';
-import { Field } from 'redux-form/immutable';
+import { Field } from 'redux-form';
+import { Field as ImmutableJsField } from 'redux-form/immutable';
 import CheckboxInputGroup from '@e-group/material/CheckboxInputGroup';
 import CheckboxInputGroupField from '@e-group/material-form/CheckboxInputGroupField';
+import ImmutableJsCheckboxInputGroupField from '@e-group/material-form/CheckboxInputGroupField/immutable';
 
 import { fromJS } from 'immutable';
 import { immutableJsStore } from '../redux/immutableJsConfigureStore';
+import { store } from '../redux/configureStore';
 import { storiesOf } from '@storybook/react';
 import { boolean, text } from '@storybook/addon-knobs';
 import checkboxInputGroupText from './checkboxInputGroup.md';
 
 storiesOf('CheckboxInputGroup', module)
-  .addDecorator(story => <Provider store={immutableJsStore}>{story()}</Provider>)
+  .addDecorator(story => <Provider store={store}>{story()}</Provider>)
   .add(
     'default',
     () => {
@@ -85,12 +89,12 @@ storiesOf('CheckboxInputGroup', module)
           }
         });
         const handleChange = values => {
-          setValues(values.toJS());
+          setValues(values);
         };
         return (
           <Grid container>
             <Grid item xs={6}>
-              <ImmutableJsReduxForm onChange={handleChange} initialValues={fromJS(values)}>
+              <ReduxForm onChange={handleChange} initialValues={values}>
                 <Field
                   name="field1"
                   label="with Field"
@@ -172,6 +176,173 @@ storiesOf('CheckboxInputGroup', module)
                   name="field3"
                   label="with Field"
                   component={CheckboxInputGroupField}
+                  helperText="please select items"
+                  fullWidth
+                  margin="normal"
+                  options={[
+                    {
+                      key: "checkbox1",
+                      name: 'checkbox1',
+                      label: 'normal checkbox',
+                      MuiCheckboxProps: {
+                        color: 'primary'
+                      }
+                    },
+                    {
+                      key: "checkbox2",
+                      name: 'checkbox2',
+                      label: 'checked with text input',
+                      MuiCheckboxProps: {
+                        color: 'primary'
+                      },
+                      toggleInput: true
+                    },
+                    {
+                      key: "checkbox3",
+                      name: 'checkbox3',
+                      label: 'checked with text input',
+                      toggleInput: true
+                    }
+                  ]}
+                  /* Pass meta props cause the failed prop type and don't worry it's just for demo */
+                  meta={{
+                    invalid: true,
+                    touched: true,
+                    error: 'fill in this option is required!'
+                  }}
+                />
+              </ReduxForm>
+            </Grid>
+            <Grid item xs={6}>
+              <Highlight
+                code={JSON.stringify(values, null, 4)}
+                type="language-json"
+              />
+            </Grid>
+          </Grid>
+        );
+      };
+      return <Form />;
+    },
+    {
+      notes: checkboxInputGroupText,
+      info: {
+        propTables: [CheckboxInputGroup],
+        propTablesExclude: [Provider]
+      }
+    }
+  );
+
+storiesOf('CheckboxInputGroup', module)
+  .addDecorator(story => <Provider store={immutableJsStore}>{story()}</Provider>)
+  .add(
+    'with ImmutableJS Field',
+    () => {
+      const Form = () => {
+        const [values, setValues] = React.useState({
+          field1: {
+            checkbox2: {
+              checked: true,
+              text: 'awesome!'
+            }
+          },
+          field2: {
+            Monday: {
+              checked: true
+            },
+            Tuesday: {
+              checked: true
+            }
+          }
+        });
+        const handleChange = values => {
+          setValues(values.toJS());
+        };
+        return (
+          <Grid container>
+            <Grid item xs={6}>
+              <ImmutableJsReduxForm onChange={handleChange} initialValues={fromJS(values)}>
+                <ImmutableJsField
+                  name="field1"
+                  label="with Field"
+                  component={ImmutableJsCheckboxInputGroupField}
+                  helperText="please select items"
+                  fullWidth
+                  margin="normal"
+                  options={[
+                    {
+                      key: "checkbox1",
+                      name: 'checkbox1',
+                      label: 'normal checkbox',
+                      MuiCheckboxProps: {
+                        color: 'primary'
+                      }
+                    },
+                    {
+                      key: "checkbox2",
+                      name: 'checkbox2',
+                      label: 'checked with text input',
+                      MuiCheckboxProps: {
+                        color: 'primary'
+                      },
+                      toggleInput: true
+                    },
+                    {
+                      key: "checkbox3",
+                      name: 'checkbox3',
+                      label: 'checked with text input',
+                      toggleInput: true
+                    }
+                  ]}
+                />
+                <ImmutableJsField
+                  name="field2"
+                  label="with Field"
+                  component={ImmutableJsCheckboxInputGroupField}
+                  fullWidth
+                  margin="normal"
+                  options={[
+                    {
+                      key: "Monday",
+                      name: 'Monday',
+                      label: 'Monday'
+                    },
+                    {
+                      key: "Tuesday",
+                      name: 'Tuesday',
+                      label: 'Tuesday'
+                    },
+                    {
+                      key: "Wednesday",
+                      name: 'Wednesday',
+                      label: 'Wednesday'
+                    },
+                    {
+                      key: "Thursday",
+                      name: 'Thursday',
+                      label: 'Thursday'
+                    },
+                    {
+                      key: "Friday",
+                      name: 'Friday',
+                      label: 'Friday'
+                    },
+                    {
+                      key: "Saturday",
+                      name: 'Saturday',
+                      label: 'Saturday'
+                    },
+                    {
+                      key: "Sunday",
+                      name: 'Sunday',
+                      label: 'Sunday'
+                    }
+                  ]}
+                />
+                <ImmutableJsField
+                  name="field3"
+                  label="with Field"
+                  component={ImmutableJsCheckboxInputGroupField}
                   helperText="please select items"
                   fullWidth
                   margin="normal"
