03ae334d91fd901477b2402ce7e89cb3104aeec4
SP_Penguin <abrcdf1023@gmail.com>
2020-05-27 10:04:34 +0800

    [story] update radio

--- stories/radio.stories/radio.stories.jsx
+++ stories/radio.stories/radio.stories.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { fromJS } from 'immutable';
+import { store } from '../redux/configureStore';
 import { store as immutableJsStore } from '../redux/immutable/configureStore';
 import radioMarkdownText from './radio.md';
 
@@ -13,9 +14,11 @@ import RadioField from '@e-group/material-form/RadioField';
 import Highlight from '../components/Highlight';
 import ImmutableJsReduxForm from '../components/immutable/ReduxForm';
 import RadioButtons from './RadioButtons';
+import { Field } from 'redux-form';
+import ReduxForm from '../components/ReduxForm';
 
 storiesOf('Radio', module)
-  .addDecorator(story => <Provider store={immutableJsStore}>{story()}</Provider>)
+  .addDecorator(story => <Provider store={store}>{story()}</Provider>)
   .add(
     'default',
     () => {
@@ -31,6 +34,56 @@ storiesOf('Radio', module)
   )
   .add(
     'with Field',
+    () => {
+      const Form = () => {
+        const [values, setValues] = React.useState({
+          gender: 'male'
+        });
+        const handleChange = values => {
+          setValues(values);
+        };
+        return (
+          <Grid container>
+            <Grid item xs={6}>
+              <ReduxForm onChange={handleChange} initialValues={values}>
+                <Field
+                  name="gender"
+                  component={RadioField}
+                  label="male"
+                  radioValue="male"
+                />
+                <Field
+                  name="gender"
+                  component={RadioField}
+                  label="female"
+                  radioValue="female"
+                />
+              </ReduxForm>
+            </Grid>
+            <Grid item xs={6}>
+              <Highlight
+                code={JSON.stringify(values, null, 4)}
+                type="language-json"
+              />
+            </Grid>
+          </Grid>
+        );
+      };
+      return <Form />;
+    },
+    {
+      notes: radioMarkdownText,
+      info: {
+        propTables: [Radio],
+        propTablesExclude: [Provider]
+      }
+    }
+  );
+
+storiesOf('Radio', module)
+  .addDecorator(story => <Provider store={immutableJsStore}>{story()}</Provider>)
+  .add(
+    'with immutableJS Field',
     () => {
       const Form = () => {
         const [values, setValues] = React.useState({
