afaa773b44ecee1bd337d4e8364377ec775fefc6
SP_Penguin <abrcdf1023@gmail.com>
2020-05-27 10:20:04 +0800

    [form] update RadioInputField

--- packages/material-form/src/RadioInputField/RadioInputField.jsx
+++ packages/material-form/src/RadioInputField/RadioInputField.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { fromJS, isImmutable } from 'immutable';
 
 import RadioInput from '@e-group/material/RadioInput';
 
@@ -11,42 +10,35 @@ const RadioInputField = ({
   MuiInputProps,
   ...other
 }) => {
-  const valueIsImmutable = isImmutable(value);
   const { onChange: inputOnChange, value: inputValue, ...otherMuiInputProps } =
     MuiInputProps || {};
 
   const handleChange = e => {
-    if (valueIsImmutable) {
-      onChange(value.set('value', e.target.value));
-    } else {
-      onChange(
-        fromJS({
-          value: e.target.value
-        })
-      );
-    }
+    onChange({
+      ...value,
+      value: e.target.value
+    });
   };
 
   const handleInputChange = e => {
-    if (valueIsImmutable) {
-      onChange(value.set('text', e.target.value));
-    } else {
-      onChange(
-        fromJS({
-          text: e.target.value
-        })
-      );
-    }
+    onChange({
+      ...value,
+      text: e.target.value
+    });
   };
+
+  const hasValue = typeof value !== 'undefined';
+  const hasRadioValue = hasValue && typeof value.value !== 'undefined';
+  const hasText = hasValue && typeof value.text !== 'undefined';
 
   return (
     <RadioInput
       onChange={handleChange}
       value={radioValue}
-      checked={valueIsImmutable ? radioValue === value.get('value') : false}
+      checked={hasRadioValue ? radioValue === value.value : false}
       MuiInputProps={{
         onChange: handleInputChange,
-        value: valueIsImmutable ? value.get('text', '') : '',
+        value: hasText ? value.text : '',
         ...otherMuiInputProps
       }}
       {...other}
--- stories/radioInput.stories/radioInput.stories.jsx
+++ stories/radioInput.stories/radioInput.stories.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { fromJS } from 'immutable';
 import { storiesOf } from '@storybook/react';
+import { store } from '../redux/configureStore';
 import { store as immutableJsStore } from '../redux/immutable/configureStore';
 import { boolean } from '@storybook/addon-knobs';
 import markdownText from './radioInput.md';
@@ -11,12 +12,15 @@ import ImmutableJsReduxForm from '../components/immutable/ReduxForm';
 import Highlight from '../components/Highlight';
 import Grid from '@material-ui/core/Grid';
 import { Field as ImmutableJsField } from 'redux-form/immutable';
-import RadioInput from '@e-group/material/RadioInput/RadioInput';
+import RadioInput from '@e-group/material/RadioInput';
 import RadioInputField from '@e-group/material-form/RadioInputField';
+import ImmutableRadioInputField from '@e-group/material-form/immutable/RadioInputField';
 import RadioInputButtons from './RadioInputButtons'
+import { Field } from 'redux-form';
+import ReduxForm from '../components/ReduxForm';
 
 storiesOf('RadioInput', module)
-  .addDecorator(story => <Provider store={immutableJsStore}>{story()}</Provider>)
+  .addDecorator(story => <Provider store={store}>{story()}</Provider>)
   .add(
     'default',
     () => {
@@ -64,6 +68,59 @@ storiesOf('RadioInput', module)
           }
         });
         const handleChange = values => {
+          setValues(values);
+        };
+        return (
+          <Grid container>
+            <Grid item xs={6}>
+              <ReduxForm onChange={handleChange} initialValues={values}>
+                <Field
+                  name="gender"
+                  component={RadioInputField}
+                  toggleInput
+                  label="male"
+                  radioValue="male"
+                />
+                <Field
+                  name="gender"
+                  component={RadioInputField}
+                  toggleInput
+                  label="female"
+                  radioValue="female"
+                />
+              </ReduxForm>
+            </Grid>
+            <Grid item xs={6}>
+              <Highlight
+                code={JSON.stringify(values, null, 4)}
+                type="language-json"
+              />
+            </Grid>
+          </Grid>
+        );
+      };
+      return <Form />;
+    },
+    {
+      notes: markdownText,
+      info: {
+        propTables: [RadioInput]
+      }
+    }
+  );
+
+storiesOf('RadioInput', module)
+  .addDecorator(story => <Provider store={immutableJsStore}>{story()}</Provider>)
+  .add(
+    'with immutable Field',
+    () => {
+      const Form = () => {
+        const [values, setValues] = React.useState({
+          gender: {
+            value: "female"
+          }
+        });
+        const handleChange = values => {
           setValues(values.toJS());
         };
         return (
@@ -72,14 +129,14 @@ storiesOf('RadioInput', module)
               <ImmutableJsReduxForm onChange={handleChange} initialValues={fromJS(values)}>
                 <ImmutableJsField
                   name="gender"
-                  component={RadioInputField}
+                  component={ImmutableRadioInputField}
                   toggleInput
                   label="male"
                   radioValue="male"
                 />
                 <ImmutableJsField
                   name="gender"
-                  component={RadioInputField}
+                  component={ImmutableRadioInputField}
                   toggleInput
                   label="female"
                   radioValue="female"
