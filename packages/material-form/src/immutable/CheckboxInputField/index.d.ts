745ee38923daaaf53f8ffd954803337cb8b8181e
SP_Penguin <abrcdf1023@gmail.com>
2020-05-27 10:11:14 +0800

    [form] update immutable type path

---
+++ packages/material-form/src/immutable/CheckboxInputField/index.d.ts
@@ -0,0 +1,2 @@
+export { default } from '../../CheckboxInputField';
+export * from '../../CheckboxInputField';
---
+++ packages/material-form/src/immutable/RadioInputField/index.d.ts
@@ -0,0 +1,2 @@
+export { default } from '../../RadioInputField';
+export * from '../../RadioInputField';
