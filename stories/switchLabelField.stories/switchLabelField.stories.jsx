4612fa5e428a656b76c17218fd1788d5242467c3
SP_Penguin <abrcdf1023@gmail.com>
2020-05-27 11:24:33 +0800

    [story] update SwitchLabelField

--- stories/switchLabelField.stories/switchLabelField.stories.jsx
+++ stories/switchLabelField.stories/switchLabelField.stories.jsx
@@ -1,19 +1,64 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import ImmutableReduxForm from '../components/immutable/ReduxForm';
-import Highlight from '../components/Highlight';
-import Grid from '@material-ui/core/Grid';
-import { Field as ImmutableField } from 'redux-form/immutable';
-import SwitchLabelField from '@e-group/material-form/SwitchLabelField';
 
 import { storiesOf } from '@storybook/react';
 import { fromJS } from 'immutable';
+import { store } from '../redux/configureStore';
 import { store as immutableStore } from '../redux/immutable/configureStore';
 
+import { Provider } from 'react-redux';
+import ReduxForm from '../components/ReduxForm';
+import ImmutableReduxForm from '../components/immutable/ReduxForm';
+import Highlight from '../components/Highlight';
+import Grid from '@material-ui/core/Grid';
+import { Field } from 'redux-form';
+import { Field as ImmutableField } from 'redux-form/immutable';
+import SwitchLabelField from '@e-group/material-form/SwitchLabelField';
+
+storiesOf('SwitchLabelField', module)
+  .addDecorator(story => <Provider store={store}>{story()}</Provider>)
+  .add(
+    'with Field',
+    () => {
+      const Form = () => {
+        const [values, setValues] = React.useState({
+          field1: 'value1',
+        });
+        const handleChange = values => {
+          setValues(values);
+        };
+        return (
+          <Grid container>
+            <Grid item xs={6}>
+              <ReduxForm onChange={handleChange} initialValues={values}>
+                <Field
+                  name="field1"
+                  component={SwitchLabelField}
+                  switchValue="value1"
+                  label="field1"
+                />
+                <Field
+                  name="field2"
+                  component={SwitchLabelField}
+                  switchValue="value2"
+                />
+              </ReduxForm>
+            </Grid>
+            <Grid item xs={6}>
+              <Highlight
+                code={JSON.stringify(values, null, 4)}
+                type="language-json"
+              />
+            </Grid>
+          </Grid>
+        );
+      };
+      return <Form />;
+    }
+  )
 storiesOf('SwitchLabelField', module)
   .addDecorator(story => <Provider store={immutableStore}>{story()}</Provider>)
   .add(
-    'default',
+    'with immutable Field',
     () => {
       const Form = () => {
         const [values, setValues] = React.useState({
