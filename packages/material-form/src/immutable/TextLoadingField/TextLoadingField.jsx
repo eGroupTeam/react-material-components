3ffdcbac6473e1fab3fe639aa48dd6a526a0e84b
SP_Penguin <abrcdf1023@gmail.com>
2020-05-27 11:25:29 +0800

    [form] add immutable TextLoadingField

---
+++ packages/material-form/src/immutable/TextLoadingField/TextLoadingField.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import TextLoading from '@e-group/material/TextLoading';
+import { fromJS } from 'immutable';
+
+export default class TextLoadingField extends Component {
+  static propTypes = {
+    /**
+     * redux from props
+     */
+    input: PropTypes.object.isRequired,
+    meta: PropTypes.object.isRequired
+  };
+
+  handleMultipleSelectOnChange = e => {
+    this.props.input.onChange(fromJS(e.target.value));
+  };
+
+  getInput() {
+    const { input, select, SelectProps } = this.props;
+    if (select && SelectProps && SelectProps.multiple) {
+      return {
+        value: input.value ? input.value.toJS() : [],
+        onChange: this.handleMultipleSelectOnChange
+      };
+    }
+    return input;
+  }
+
+  render() {
+    const {
+      meta: { touched, error, invalid, asyncValidating },
+      error: errorProp,
+      helperText,
+      InputProps,
+      ...other
+    } = this.props;
+    const isError = touched && invalid;
+    return (
+      <TextLoading
+        error={isError}
+        loading={asyncValidating}
+        helperText={isError ? error : helperText}
+        InputProps={InputProps}
+        {...this.getInput()}
+        {...other}
+      />
+    );
+  }
+}
