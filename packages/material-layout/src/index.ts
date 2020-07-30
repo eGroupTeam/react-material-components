7e934cc3b4fd7a601dbeda0d8be2b739ee1b6df2
SP_Penguin <abrcdf1023@gmail.com>
2020-07-30 17:30:59 +0800

    [layout] update Main

--- packages/material-layout/src/index.ts
+++ packages/material-layout/src/index.ts
@@ -3,3 +3,6 @@ export * from './Center';
 
 export { default as FixedCenter } from './FixedCenter';
 export * from './FixedCenter';
+
+export { default as Main } from './Main';
+export * from './Main';
