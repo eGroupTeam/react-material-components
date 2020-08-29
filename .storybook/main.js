9f25081b197193646379a6eecc28ff40c73e357f
SP_Penguin <abrcdf1023@gmail.com>
2020-08-29 18:17:58 +0800

    [story] chore upgrade to v6

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,18 +2,34 @@ const path = require('path');
 
 // Export a function. Accept the base config as the only param.
 module.exports = {
-  addons: ['@storybook/preset-typescript'],
+  stories: ['../stories/**/*.stories.@(tsx|ts|jsx|js|mdx)'],
+  addons: [
+    '@storybook/addon-knobs/register',
+    {
+      name: '@storybook/addon-storysource',
+      options: {
+        rule: {
+          // test: [/\.stories\.jsx?$/], This is default
+          enforce: 'pre'
+        },
+      },
+    },
+    '@storybook/addon-actions/register',
+    '@storybook/addon-links/register'
+  ],
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+    },
+  },
   webpackFinal: async (config, { configType }) => {
     // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
     // You can change the configuration based on that.
     // 'PRODUCTION' is used when building the static version of storybook.
-
-    // Make whatever fine-grained changes you need
-    config.module.rules.push({
-      test: /\.stories\.jsx?$/,
-      loaders: [require.resolve('@storybook/addon-storysource/loader')],
-      enforce: 'pre'
-    });
 
     config.resolve.alias = {
       '@e-group/material': path.resolve(__dirname, '../packages/material/src/'),
