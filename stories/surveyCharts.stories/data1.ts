74f30fab99953134b14dcbb51a79e3d0e4d233ba
SP_Penguin <abrcdf1023@gmail.com>
2020-08-31 18:02:22 +0800

    [story] feat update SurveyCharts test case

---
+++ stories/surveyCharts.stories/data1.ts
@@ -0,0 +1,49 @@
+const data = [
+  {
+    "questionId": "af9280403e634695aeede980d4afca0c",
+    "questionName": "123456",
+    "questionNo": 2,
+    "questionType": "rating",
+    "questionRatingStartValue": 0,
+    "questionRatingEndValue": 5,
+    "responseContentList": [
+      {
+        "responseContent": "2",
+        "responseContentCount": 1
+      }
+    ],
+    "questionCount": 1
+  },
+  {
+    "questionId": "b787c0f026a045679cd6c0d9213ec0ba",
+    "questionName": "132456",
+    "questionNo": 3,
+    "questionType": "rating",
+    "questionRatingStartValue": 0,
+    "questionRatingEndValue": 5,
+    "responseContentList": [
+      {
+        "responseContent": "3",
+        "responseContentCount": 1
+      }
+    ],
+    "questionCount": 1
+  },
+  {
+    "questionId": "cf6880cb85c344ab838d6ab7a4eeda9c",
+    "questionName": "789456132",
+    "questionNo": 3,
+    "questionType": "text",
+    "questionRatingStartValue": 0,
+    "questionRatingEndValue": 0,
+    "responseContentList": [
+      {
+        "responseContent": "123456",
+        "responseContentCount": 1
+      }
+    ],
+    "questionCount": 1
+  }
+]
+
+export default data
--- stories/surveyCharts.stories/surveyCharts.stories.tsx
+++ stories/surveyCharts.stories/surveyCharts.stories.tsx
@@ -3,22 +3,28 @@ import { storiesOf } from '@storybook/react';
 
 import SurveyCharts from '@e-group/material-module/SurveyCharts';
 import data from './data'
+import data1 from './data1'
 
 storiesOf('SurveyCharts', module)
   .add(
     'default',
     () => {
-      const Demo = () => (
+      return (
         <SurveyCharts
           data={data}
           totalResponses={15}
         />
       )
-      return <Demo />
     },
-    {
-      info: {
-        propTables: [SurveyCharts]
-      }
-    }
+  )
+  .add(
+    'with rating',
+    () => {
+      return (
+        <SurveyCharts
+          data={data1}
+          totalResponses={1}
+        />
+      )
+    },
   )
