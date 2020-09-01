1f389c7a2886fc008ad002da0ffe3029781e7f0c
SP_Penguin <abrcdf1023@gmail.com>
2020-09-01 18:16:18 +0800

    [story] refactor textLoading story

---
+++ stories/textLoading.stories/TextLoading.stories.tsx
@@ -0,0 +1,171 @@
+import React from 'react';
+
+import { Meta } from '@storybook/react';
+import { store } from '../redux/configureStore';
+
+import { Provider } from 'react-redux';
+import ReduxForm from '../components/ReduxForm';
+import Highlight from '../components/Highlight';
+import Grid from '@material-ui/core/Grid';
+import { Field } from 'redux-form';
+import TextLoading from '@e-group/material/TextLoading';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import TextLoadingField from '@e-group/material-form/TextLoadingField';
+
+export default {
+  title: 'Components/TextLoading',
+  component: TextLoading,
+} as Meta;
+
+export const Default: React.FC<{}> = () => (
+  <TextLoading
+    loading
+    label="default"
+    fullWidth
+    helperText="account is validating..."
+    margin="normal"
+    required
+  />
+);
+
+export const WithCustomizedLoadingAdornment: React.FC<{}> = () => (
+  <TextLoading
+    label="with customized loadingAdornment"
+    fullWidth
+    loading
+    loadingAdornment={
+      <InputAdornment position="end">
+        <CircularProgress color="secondary" size={20} />
+      </InputAdornment>
+    }
+    helperText="If set loading=`true` the endAdornment will be replaced by loadingAdornment"
+    margin="normal"
+    InputProps={{
+      endAdornment: <InputAdornment position="end">Kg</InputAdornment>
+    }}
+    required
+  />
+);
+
+export const WithSelect: React.FC<{}> = () => {
+  const [value, setValue] = React.useState('option1');
+  const handleChange = (e: any) => {
+    setValue(e.target.value);
+  };
+  return (
+    <TextLoading
+      label="with Select"
+      fullWidth
+      loading
+      value={value}
+      select
+      margin="normal"
+      onChange={handleChange}
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">Kg</InputAdornment>
+        )
+      }}
+      required
+    >
+      <MenuItem value="option1">option1</MenuItem>
+      <MenuItem value="option2">option2</MenuItem>
+    </TextLoading>
+  );
+}
+
+export const WithReduxFormField: React.FC<{}> = () => {
+  const [values, setValues] = React.useState({
+    field1: 'admin@gmail.com'
+  });
+  const handleChange = (values: any) => {
+    setValues(values);
+  };
+  return (
+    <Provider store={store}>
+      <Grid container>
+        <Grid item xs={6}>
+          <ReduxForm onChange={handleChange} initialValues={values}>
+            <Field
+              label="default"
+              name="field1"
+              margin="normal"
+              component={TextLoadingField}
+              fullWidth
+            />
+            {/* Pass meta props cause the failed prop type and don't worry it's just for demo */}
+            <Field
+              label="loading"
+              name="field2"
+              margin="normal"
+              component={TextLoadingField}
+              fullWidth
+              meta={{ asyncValidating: true }}
+            />
+            {/* Pass meta props cause the failed prop type and don't worry it's just for demo */}
+            <Field
+              label="error"
+              name="field3"
+              margin="normal"
+              required
+              component={TextLoadingField}
+              fullWidth
+              meta={{
+                invalid: true,
+                touched: true,
+                error: 'error message'
+              }}
+            />
+            <Field
+              label="select"
+              name="field4"
+              fullWidth
+              loading
+              component={TextLoadingField}
+              select={true}
+              margin="normal"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">Kg</InputAdornment>
+                )
+              }}
+              required
+            >
+              <MenuItem value="option1">option1</MenuItem>
+              <MenuItem value="option2">option2</MenuItem>
+            </Field>
+            <Field
+              label="multiple select"
+              name="field5"
+              SelectProps={{
+                multiple: true
+              }}
+              fullWidth
+              loading
+              component={TextLoadingField}
+              select={true}
+              margin="normal"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">Kg</InputAdornment>
+                )
+              }}
+              required
+            >
+              <MenuItem value="option1">option1</MenuItem>
+              <MenuItem value="option2">option2</MenuItem>
+            </Field>
+          </ReduxForm>
+        </Grid>
+        <Grid item xs={6}>
+          <Highlight
+            code={JSON.stringify(values, null, 4)}
+            type="language-json"
+          />
+        </Grid>
+      </Grid>
+    </Provider>
+  );
+}
---
+++ stories/textLoading.stories/TextLoadingFormik.stories.tsx
@@ -0,0 +1,103 @@
+import React from 'react';
+
+import { Meta } from '@storybook/react';
+
+import { Formik, Form, Field} from 'formik'
+import Highlight from '../components/Highlight';
+import { Grid, MenuItem, Button, InputAdornment } from '@material-ui/core';
+import TextLoading from '@e-group/material/TextLoading';
+import TextLoadingField from '@e-group/material-formik/TextLoadingField';
+
+export default {
+  title: 'Components/TextLoading',
+  component: TextLoading,
+} as Meta;
+
+export const WithFormikField: React.FC<{}> = () => {
+  const [values, setValues] = React.useState({
+    field1: 'admin@gmail.com',
+    field3: '',
+    field4: '',
+    field5: []
+  });
+  const handleSubmit = (values: any) => {
+    setValues(values);
+  };
+  
+  return (
+    <Grid container>
+      <Grid item xs={6}>
+        <Formik onSubmit={handleSubmit} initialValues={values}>
+          <Form>
+            <Field
+              label="default"
+              name="field1"
+              margin="normal"
+              component={TextLoadingField}
+              fullWidth
+            />
+            {/* Pass form props cause the failed prop type and don't worry it's just for demo */}
+            <Field
+              label="error"
+              name="field3"
+              margin="normal"
+              component={TextLoadingField}
+              fullWidth
+              form={{
+                isValid: false,
+                touched: true,
+                errors: {
+                  field3: 'error message'
+                }
+              }}
+            />
+            <Field
+              label="select"
+              name="field4"
+              fullWidth
+              loading
+              component={TextLoadingField}
+              select={true}
+              margin="normal"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">Kg</InputAdornment>
+                )
+              }}
+            >
+              <MenuItem value="option1">option1</MenuItem>
+              <MenuItem value="option2">option2</MenuItem>
+            </Field>
+            <Field
+              label="multiple select"
+              name="field5"
+              SelectProps={{
+                multiple: true
+              }}
+              fullWidth
+              loading
+              component={TextLoadingField}
+              select={true}
+              margin="normal"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">Kg</InputAdornment>
+                )
+              }}
+            >
+              <MenuItem value="option1">option1</MenuItem>
+              <MenuItem value="option2">option2</MenuItem>
+            </Field>
+            <Button type="submit">Submit</Button>
+          </Form>
+        </Formik>
+      </Grid>
+      <Grid item xs={6}>
+        <Highlight
+          code={JSON.stringify(values, null, 4)}
+          type="language-json"
+        />
+      </Grid>
+    </Grid>
+  )
+}
