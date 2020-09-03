c84bb504de89948aca07204f30ad3ede2e5e5e1c
SP_Penguin <abrcdf1023@gmail.com>
2020-09-03 10:26:12 +0800

    [chore] fixed ts declaration build config

--- packages/material-module/src/SurveyCharts/index.ts
+++ packages/material-module/src/SurveyCharts/index.ts
@@ -1,1 +1,2 @@
 export { default } from './SurveyCharts';
+export * from './SurveyCharts';
--- packages/material-module/src/index.ts
+++ packages/material-module/src/index.ts
@@ -1,3 +1,7 @@
+// to solve typescript declarations problem
+// https://github.com/mui-org/material-ui-pickers/issues/1074
+import '@date-io/date-fns/type';
+
 export { default as Picker } from './Picker';
 export * from './Picker';
 export { default as ReactSelect } from './ReactSelect';
