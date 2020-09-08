1d43b4720143bc1430bc5ae08b4cc791eabc9948
SP_Penguin <abrcdf1023@gmail.com>
2020-09-08 10:50:34 +0800

    [formik] feat useField utils

---
+++ packages/material-formik/src/utils/useField.ts
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { getIn, FieldInputProps, FormikProps } from 'formik';
+
+export default function useField(
+  field: FieldInputProps<any>,
+  form: FormikProps<any>,
+  disabledProp?: boolean
+) {
+  const { isSubmitting, touched, errors, submitCount, setFieldTouched } = form;
+  const fieldError = getIn(errors, field.name);
+  const showError = getIn(touched, field.name) && !!fieldError;
+  const disabled = disabledProp ?? isSubmitting;
+
+  // Need this to solve touched issue.
+  // https://github.com/formium/formik/issues/445
+  React.useEffect(() => {
+    if (submitCount > 0) {
+      setFieldTouched(field.name);
+    }
+  }, [field.name, setFieldTouched, submitCount]);
+
+  return {
+    fieldError,
+    showError,
+    disabled
+  };
+}
