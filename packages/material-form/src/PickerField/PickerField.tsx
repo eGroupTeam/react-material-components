3ad886abb693631bedea056b31e9d46993415811
SP_Penguin <abrcdf1023@gmail.com>
2020-09-10 18:28:22 +0800

    [form] fixed PickerField

--- packages/material-form/src/PickerField/PickerField.tsx
+++ packages/material-form/src/PickerField/PickerField.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import Picker, { PickerProps } from '@e-group/material-module/Picker';
 import { WrappedFieldProps } from 'redux-form';
+import { MaterialUiPickersDate } from '@material-ui/pickers/typings/date';
 
 export interface BasePickerFieldProps extends WrappedFieldProps {
   /**
@@ -12,21 +13,31 @@ export interface BasePickerFieldProps extends WrappedFieldProps {
 export type PickerFieldProps = BasePickerFieldProps & PickerProps;
 
 const PickerField: FC<PickerFieldProps> = ({
-  input: { value, ...otherInput },
+  input: { value, onChange, onBlur, onDragStart, onDrop, onFocus, ...input },
   meta: { touched, error, invalid },
   pickerFormat,
   error: errorProp,
   helperText,
+  onChange: onChangeProp,
   ...other
 }) => {
   const isError = touched && invalid;
+
+  const handleChange = (date: MaterialUiPickersDate) => {
+    if (onChangeProp) {
+      onChangeProp(date);
+    }
+    onChange(date);
+  };
+
   return (
     <Picker
       format={pickerFormat}
       error={isError}
       helperText={isError ? error : helperText}
       value={value === '' ? null : value}
-      {...otherInput}
+      onChange={handleChange}
+      {...input}
       {...other}
     />
   );
