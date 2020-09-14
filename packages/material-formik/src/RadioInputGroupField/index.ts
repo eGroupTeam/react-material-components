715c7aa138a038466a60622076bc331def78cbe3
SP_Penguin <abrcdf1023@gmail.com>
2020-09-14 18:51:24 +0800

    fixed missing export

--- packages/material-formik/src/RadioInputGroupField/index.ts
+++ packages/material-formik/src/RadioInputGroupField/index.ts
@@ -1,1 +1,2 @@
 export { default } from './RadioInputGroupField';
+export * from './RadioInputGroupField';
--- packages/material-formik/src/TextLoadingField/index.ts
+++ packages/material-formik/src/TextLoadingField/index.ts
@@ -1,1 +1,2 @@
 export { default } from './TextLoadingField';
+export * from './TextLoadingField';
