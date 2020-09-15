2c3f16d4ab78d7b6be2f478c71a085a80982683e
SP_Penguin <abrcdf1023@gmail.com>
2020-09-15 16:54:50 +0800

    [chore] fixed travis lifecycle and jest config

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,15 @@ module.exports = {
   preset: 'ts-jest/presets/js-with-babel',
   coverageDirectory: './coverage/',
   collectCoverage: true,
-  modulePathIgnorePatterns: ['build', 'node_modules']
+  // ignore these build, dist & library directories
+  coveragePathIgnorePatterns: ['/node_modules/', '/dist/', '/build/'],
+  modulePathIgnorePatterns: [
+    '<rootDir>/packages/material/build',
+    '<rootDir>/packages/material-form/build',
+    '<rootDir>/packages/material-formik/build',
+    '<rootDir>/packages/material-icons/build',
+    '<rootDir>/packages/material-lab/build',
+    '<rootDir>/packages/material-layout/build',
+    '<rootDir>/packages/material-module/build'
+  ]
 };
