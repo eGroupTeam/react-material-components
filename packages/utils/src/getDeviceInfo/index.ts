597214ce9caa97298b189af33b6c619155933649
SP_Penguin <abrcdf1023@gmail.com>
2020-09-17 17:32:01 +0800

    [utils] fixed export

--- packages/utils/src/getDeviceInfo/index.ts
+++ packages/utils/src/getDeviceInfo/index.ts
@@ -1,1 +1,2 @@
 export { default } from './getDeviceInfo';
+export * from './getDeviceInfo';
--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -4,7 +4,6 @@ export * from './twIdValidator';
 export { default as makeBasicFetchEpic } from './makeBasicFetchEpic';
 export * from './makeBasicFetchEpic';
 export { default as createObservableApi } from './createObservableApi';
-export * from './createObservableApi';
 export { default as validator } from './validator';
 export * from './validator';
 export * from './dateUtils';
