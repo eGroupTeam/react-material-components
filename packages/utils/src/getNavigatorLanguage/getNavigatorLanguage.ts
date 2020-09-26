e93b2562164cceb3790553cbff37d14440e29544
SP_Penguin <abrcdf1023@gmail.com>
2020-09-26 20:28:31 +0800

    [utils] fixed getNavigatorLanguage SSR

--- packages/utils/src/getNavigatorLanguage/getNavigatorLanguage.ts
+++ packages/utils/src/getNavigatorLanguage/getNavigatorLanguage.ts
@@ -1,4 +1,7 @@
+const isBrowser = typeof navigator !== 'undefined';
+
 export default function getNavigatorLanguage() {
+  if (!isBrowser) return 'en';
   if (navigator.languages && navigator.languages.length) {
     return navigator.languages[0].toLowerCase();
   }
