191b0f12852e73499f9213ef001695c5638198f3
SP_Penguin <abrcdf1023@gmail.com>
2020-09-26 17:27:47 +0800

    [hooks] refactor useLocationSearch to ts

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,3 +1,4 @@
 export { default as useScrollPosition } from './useScrollPosition';
 export * from './useScrollPosition';
 export { default as useTab } from './useTab';
+export { default as useLocationSearch } from './useLocationSearch';
--- packages/hooks/src/useLocationSearch.ts
+++ packages/hooks/src/useLocationSearch.ts
@@ -2,7 +2,7 @@ import React from 'react';
 
 import queryString from 'query-string';
 
-export default function useLocationSearch(location) {
+export default function useLocationSearch(location: Location) {
   const search = React.useMemo(() => queryString.parse(location.search), [
     location.search,
   ]);
