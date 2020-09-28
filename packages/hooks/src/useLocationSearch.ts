b47cb77bf2f9022dfbbcdeca04422ab5ef260bfa
SP_Penguin <abrcdf1023@gmail.com>
2020-09-28 20:33:17 +0800

    [hooks] chore useLocationSearch minor change

--- packages/hooks/src/useLocationSearch.ts
+++ packages/hooks/src/useLocationSearch.ts
@@ -1,9 +1,9 @@
-import React from 'react';
+import { useMemo } from 'react';
 
 import queryString from 'query-string';
 
 export default function useLocationSearch(location: Location) {
-  const search = React.useMemo(() => queryString.parse(location.search), [
+  const search = useMemo(() => queryString.parse(location.search), [
     location.search,
   ]);
 
