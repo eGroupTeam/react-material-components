5618dfc4ed2eb285f8f6de9911320b4f71359a91
SP_Penguin <abrcdf1023@gmail.com>
2020-10-06 11:29:06 +0800

    [utils] fixed findDeepValue type

--- packages/utils/src/findDeepValue/findDeepValue.ts
+++ packages/utils/src/findDeepValue/findDeepValue.ts
@@ -2,7 +2,7 @@
  * To solve index path bug please read this for more detail.
  * https://stackoverflow.com/questions/6393943/convert-javascript-string-in-dot-notation-into-an-object-reference
  */
-const findDeepValue = <T>(obj: T, path = '') => {
+const findDeepValue = <R>(obj: any, path = '') => {
   const matchArray = path.match(/[^\]\\[.]+/g) ?? [];
   const result = matchArray.reduce((o, i) => {
     if (o) {
@@ -13,7 +13,7 @@ const findDeepValue = <T>(obj: T, path = '') => {
   if (!result) {
     return undefined;
   }
-  return result;
+  return result as R;
 };
 
 export default findDeepValue;
