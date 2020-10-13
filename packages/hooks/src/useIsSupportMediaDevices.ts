0c267b82c333262445f621247f9898b18042fca4
SP_Penguin <abrcdf1023@gmail.com>
2020-10-13 15:26:14 +0800

    [hooks] refactor useIsSupportMediaDevices to ts 1. return info use bool attribute instead of message

---
+++ packages/hooks/src/useIsSupportMediaDevices.ts
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react';
+import MobileDetect from 'mobile-detect';
+
+const md = new MobileDetect(window.navigator.userAgent);
+
+export type SupportMediaDevicesInfo = {
+  iosVersionNotSupprot?: boolean;
+  iosBrowserNotSupport?: boolean;
+  androidBrowserNotSupport?: boolean;
+  otherBrowserNotSupport?: boolean;
+};
+/**
+ * To check user environment is supprot mediaDevices.
+ */
+export default function useIsSupportMediaDevices() {
+  const [info, setInfo] = useState<SupportMediaDevicesInfo>({});
+  const isSupportMediaDevices = !!(
+    navigator.mediaDevices && navigator.mediaDevices.getUserMedia
+  );
+
+  useEffect(() => {
+    if (isSupportMediaDevices) return;
+    const os = md.os();
+    if (os === 'iOS') {
+      if (md.version('iOS') < 11) {
+        setInfo({
+          iosVersionNotSupprot: true,
+        });
+      } else {
+        setInfo({
+          iosBrowserNotSupport: true,
+        });
+      }
+    } else if (os === 'AndroidOS') {
+      setInfo({
+        androidBrowserNotSupport: true,
+      });
+    } else {
+      setInfo({
+        otherBrowserNotSupport: true,
+      });
+    }
+  });
+
+  return [isSupportMediaDevices, info];
+}
