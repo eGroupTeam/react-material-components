e9c2767c099c687d11dcd9184e4ca55e82a94ebe
SP_Penguin <abrcdf1023@gmail.com>
2020-11-09 18:21:43 +0800

    [story] feat update YoutubePlayer example

--- stories/youtubePlayer.stories/YoutubePlayer.stories.tsx
+++ stories/youtubePlayer.stories/YoutubePlayer.stories.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React from 'react';
 
 import { Meta, Story } from '@storybook/react';
 
@@ -16,7 +16,31 @@ export default {
   },
 } as Meta;
 
-export const Default: Story<YoutubePlayerProps> = ({
+export const Default: Story<YoutubePlayerProps> = () => (
+  <div style={{ width: 600 }}>
+    <YoutubePlayer
+      placeholder="/thumb.jpg"
+      iframeSrc="https://www.youtube.com/embed/WHiQXDusD0M"
+      iframeTitle="eGroupAI │刷臉打卡系統 V2【人臉辨識 Demo】"
+    />
+    <br />
+    <YoutubePlayer
+      variant="lightbox"
+      placeholder="/thumb.jpg"
+      iframeSrc="https://www.youtube.com/embed/4nZMg3s9VGQ?start=2646"
+      iframeTitle="公共電視新聞採訪 eGroupAI"
+    />
+    <br />
+    <YoutubePlayer
+      ratio="1905:702"
+      placeholder="/thumb.jpg"
+      iframeSrc="https://www.youtube.com/embed/4nZMg3s9VGQ?start=2646"
+      iframeTitle="公共電視新聞採訪 eGroupAI"
+    />
+  </div>
+);
+
+export const WithControled: Story<YoutubePlayerProps> = ({
   placeholder,
   iframeSrc,
   iframeTitle,
