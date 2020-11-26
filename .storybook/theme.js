b117f40e84823b56c801274f25786ab97c8bdc26
SP_Penguin <abrcdf1023@gmail.com>
2020-11-26 11:47:39 +0800

    [storybook] use light theme

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -1,7 +1,7 @@
 import { create } from '@storybook/theming';
 
 export default create({
-  base: 'dark',
+  base: 'light',
 
   brandTitle: 'eGroup Material',
   brandUrl: 'https://eGroupAI.github.io/egroup-material',
