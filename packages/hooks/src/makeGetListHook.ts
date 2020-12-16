1151324a354d3630946dc8c37235410888381db9
SP_Penguin <abrcdf1023@gmail.com>
2020-12-16 15:30:13 +0800

    [hooks] fixed makeGetListHook type

--- packages/hooks/src/makeGetListHook.ts
+++ packages/hooks/src/makeGetListHook.ts
@@ -1,6 +1,7 @@
-import { AxiosInstance } from 'axios';
+import { AxiosError, AxiosInstance } from 'axios';
 import queryString from 'query-string';
 import replacer from '@e-group/utils/replacer';
+import { mutateCallback } from 'swr/dist/types';
 import useAxiosSWR from './useAxiosSWR';
 import getShouldFetch from './utils/getShouldFetch';
 
@@ -16,11 +17,22 @@ export interface PathParams {
   [key: string]: string | undefined;
 }
 
+export type UseListReturnedValues<T> = {
+  data: EntityList<T> | undefined;
+  isLoading: boolean;
+  isEmpty: boolean;
+  isError: AxiosError;
+  mutate: mutateCallback;
+};
+
 export default function makeGetListHook<T = any, P = PathParams>(
   urlPattern: string,
   fetcher: AxiosInstance
 ) {
-  return function useList(params: P, payload?: QueryPayload) {
+  return function useList(
+    params: P,
+    payload?: QueryPayload
+  ): UseListReturnedValues<T> {
     const query = payload ? queryString.stringify(payload) : '';
     const { response, data, error, mutate } = useAxiosSWR<EntityList<T>>(
       getShouldFetch(params)
