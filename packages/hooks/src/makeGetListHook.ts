3ef30e203ac45edf932e6655c1c1471373354f72
SP_Penguin <abrcdf1023@gmail.com>
2020-12-23 19:09:34 +0800

    [hook] fixed makeGetListHook type

--- packages/hooks/src/makeGetListHook.ts
+++ packages/hooks/src/makeGetListHook.ts
@@ -1,5 +1,5 @@
 import { AxiosError, AxiosInstance } from 'axios';
-import queryString from 'query-string';
+import queryString, { StringifiableRecord } from 'query-string';
 import replacer from '@e-group/utils/replacer';
 import { responseInterface } from 'swr/dist/types';
 import objectCheckNull from '@e-group/utils/objectCheckNull';
@@ -9,10 +9,6 @@ export interface EntityList<T> {
   total: number;
   source: T[];
 }
-export interface QueryPayload {
-  [key: string]: string;
-}
-
 export interface PathParams {
   [key: string]: string | undefined;
 }
@@ -31,12 +27,12 @@ export default function makeGetListHook<T = any, P = PathParams>(
 ) {
   return function useList(
     params: P,
-    payload?: QueryPayload
+    payload?: StringifiableRecord
   ): UseListReturnedValues<T> {
-    const query = payload ? queryString.stringify(payload) : '';
+    const query = payload ? `?${queryString.stringify(payload)}` : '';
     const { response, data, error, mutate } = useAxiosSWR<EntityList<T>>(
       !objectCheckNull(params)
-        ? `${replacer<P>(urlPattern, params)}?${query}`
+        ? `${replacer<P>(urlPattern, params)}${query}`
         : null,
       fetcher
     );
