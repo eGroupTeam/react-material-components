720f39c543d394f4fd7c51caf31626e40d456bc6
SP_Penguin <abrcdf1023@gmail.com>
2020-12-30 18:37:16 +0800

    [hooks] fixed useSearch export path

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -3,5 +3,5 @@ export * from './makeSearchDataList';
 export { default as useScrollPosition } from './useScrollPosition';
 export * from './useScrollPosition';
 export { default as useTab } from './useTab';
-export { default as useLocationSearch } from './useLocationSearch';
+export { default as useSearch } from './useSearch';
 export { default as usePrevious } from './usePrevious';
