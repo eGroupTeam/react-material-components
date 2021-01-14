3998cd38d782cb5e17f3cb6870b28bd51c63d46c
SP_Penguin <abrcdf1023@gmail.com>
2021-01-14 11:47:17 +0800

    [client] fixed useTab bug

--- packages/hooks/src/useTab/useTab.ts
+++ packages/hooks/src/useTab/useTab.ts
@@ -1,11 +1,14 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import cache from './cache';
 
 export default function useTab(key: string, defaultValue = 0) {
   const [value, setValue] = useState(cache.get(key) ?? defaultValue);
 
+  useEffect(() => {
+    cache.set(key, value);
+  }, [key, value]);
+
   const handleChange = (newValue: number) => {
-    cache.set(key, newValue);
     setValue(newValue);
   };
 
