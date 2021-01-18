11ddf69bc41b633ff7c22bc233368d15ab79e5da
SP_Penguin <abrcdf1023@gmail.com>
2021-01-18 17:39:59 +0800

    [hooks] feat makePostHook

---
+++ packages/hooks/src/makePostHook.ts
@@ -0,0 +1,61 @@
+import { useCallback } from 'react';
+import { AxiosError, AxiosInstance, AxiosResponse } from 'axios';
+import queryString, { StringifiableRecord } from 'query-string';
+import replacer from '@e-group/utils/replacer';
+import objectCheckNull from '@e-group/utils/objectCheckNull';
+import { responseInterface } from 'swr';
+import useAxiosSWR from './useAxiosSWR';
+
+export interface PathParams {
+  [key: string]: string | undefined;
+}
+
+export interface ReturnedValues<Data> {
+  data?: Data;
+  mutate: responseInterface<any, any>['mutate'];
+  response?: AxiosResponse<Data>;
+  error?: AxiosError;
+  isLoading: boolean;
+  isError: boolean;
+}
+
+export type UseItem<T, P> = <Data = T>(
+  params?: P,
+  payload?: StringifiableRecord
+) => ReturnedValues<Data>;
+
+export default function makePostHook<T = any, P = PathParams>(
+  urlPattern: string,
+  fetcher: AxiosInstance
+) {
+  return function useItem<Data = T>(
+    params?: P,
+    payload?: StringifiableRecord
+  ): ReturnedValues<Data> {
+    const postFetcher = useCallback(() => fetcher.post(urlPattern, payload), [
+      payload,
+    ]);
+    const query = payload ? `?${queryString.stringify(payload)}` : '';
+    const getKey = () => {
+      if (params) {
+        return !objectCheckNull(params)
+          ? `${replacer<P>(urlPattern, params)}${query}`
+          : null;
+      }
+      return `${urlPattern}${query}`;
+    };
+    const { response, data, error, mutate } = useAxiosSWR<Data>(
+      getKey(),
+      postFetcher
+    );
+
+    return {
+      data,
+      isLoading: !error && !response,
+      isError: !!error,
+      mutate,
+      response,
+      error,
+    };
+  };
+}
---
+++ stories/makePostHook.stories/MakePostHook.stories.tsx
@@ -0,0 +1,52 @@
+import React, { FC } from 'react';
+
+import axios from 'axios';
+import { Meta } from '@storybook/react';
+import makePostHook from '@e-group/hooks/makePostHook';
+import { Card, CardContent } from '@material-ui/core';
+
+export default {
+  title: 'Utils/makePostHook',
+} as Meta;
+
+const fetcher = axios.create({
+  baseURL: 'https://reqres.in/api',
+});
+
+export interface User {
+  data: Data;
+  support: Support;
+}
+
+export interface Data {
+  id: number;
+  name: string;
+  job: string;
+}
+
+export interface Support {
+  url: string;
+  text: string;
+}
+const useCreateUser = makePostHook<Data>('/users', fetcher);
+
+export const Default: FC = () => {
+  const { data } = useCreateUser(undefined, {
+    name: 'morpheus',
+    job: 'leader',
+  });
+
+  return (
+    <>
+      {data && (
+        <Card style={{ width: 240 }}>
+          <CardContent>
+            {data.name}
+            <br />
+            {data.job}
+          </CardContent>
+        </Card>
+      )}
+    </>
+  );
+};
