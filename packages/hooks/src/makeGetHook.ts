d09fc9b91a6ebfd5a871977749409fd137c2a661
SP_Penguin <abrcdf1023@gmail.com>
2021-01-18 15:52:18 +0800

    [hooks] fixed makeGetHook type

--- packages/hooks/src/makeGetHook.ts
+++ packages/hooks/src/makeGetHook.ts
@@ -1,18 +1,31 @@
-import { AxiosInstance } from 'axios';
+import { AxiosError, AxiosInstance, AxiosResponse } from 'axios';
 import queryString, { StringifiableRecord } from 'query-string';
 import replacer from '@e-group/utils/replacer';
 import objectCheckNull from '@e-group/utils/objectCheckNull';
+import { responseInterface } from 'swr';
 import useAxiosSWR from './useAxiosSWR';
 
 export interface PathParams {
   [key: string]: string | undefined;
 }
 
+export interface ReturnedValues<Data> {
+  data?: Data;
+  mutate: responseInterface<any, any>['mutate'];
+  response?: AxiosResponse<Data>;
+  error?: AxiosError;
+  isLoading: boolean;
+  isError: boolean;
+}
+
 export default function makeGetHook<T = any, P = PathParams>(
   urlPattern: string,
   fetcher: AxiosInstance
 ) {
-  return function useItem<Data = T>(params?: P, payload?: StringifiableRecord) {
+  return function useItem<Data = T>(
+    params?: P,
+    payload?: StringifiableRecord
+  ): ReturnedValues<Data> {
     const query = payload ? `?${queryString.stringify(payload)}` : '';
     const getKey = () => {
       if (params) {
@@ -30,7 +43,7 @@ export default function makeGetHook<T = any, P = PathParams>(
     return {
       data,
       isLoading: !error && !response,
-      isError: error,
+      isError: !!error,
       mutate,
       response,
       error,
