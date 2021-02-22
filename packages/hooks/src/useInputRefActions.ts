63a43e54bd7ee896e43c6fd4af516407fb72322c
SP_Penguin <abrcdf1023@gmail.com>
2021-02-22 17:24:15 +0800

    [hooks] feat useInputRefActions

---
+++ packages/hooks/src/useInputRefActions.ts
@@ -0,0 +1,30 @@
+import { useCallback, useRef } from 'react';
+
+export default function useInputRefActions() {
+  const inputEl = useRef<HTMLInputElement>();
+  const select = useCallback(() => {
+    if (inputEl.current) {
+      inputEl.current.select();
+      inputEl.current.setSelectionRange(0, 99999); /* For mobile devices */
+    }
+  }, []);
+
+  const focus = useCallback(() => {
+    if (inputEl.current) {
+      inputEl.current.focus();
+    }
+  }, []);
+
+  const clearValue = useCallback(() => {
+    if (inputEl.current) {
+      inputEl.current.value = '';
+    }
+  }, []);
+
+  return {
+    inputEl,
+    select,
+    focus,
+    clearValue,
+  };
+}
