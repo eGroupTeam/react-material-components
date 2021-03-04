02baf261b876cd229c9b7a28f6c5f4b568d49aee
SP_Penguin <abrcdf1023@gmail.com>
2021-03-04 15:23:07 +0800

    [hooks] remove UseItem type

--- packages/hooks/src/typings.ts
+++ packages/hooks/src/typings.ts
@@ -1,6 +1,5 @@
 import { AxiosError, AxiosResponse } from 'axios';
-import { StringifiableRecord } from 'query-string';
-import { ConfigInterface, responseInterface } from 'swr';
+import { responseInterface } from 'swr';
 
 export interface PathParams {
   [key: string]: string | undefined;
@@ -19,9 +18,3 @@ export interface ReturnedValues<Data, ErrorData> {
   revalidate: () => Promise<boolean>;
   isValidating: boolean;
 }
-
-export type UseItem<T, P, E> = <Data = T, ErrorData = E>(
-  params?: P,
-  payload?: StringifiableRecord,
-  config?: ConfigInterface<AxiosResponse<Data>, AxiosError<ErrorData>>
-) => ReturnedValues<Data, ErrorData>;
