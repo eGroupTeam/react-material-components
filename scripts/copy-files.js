30fdaf67d3a74f53aceff128778d7e692b757594
SP_Penguin <abrcdf1023@gmail.com>
2021-04-20 11:31:41 +0800

    [devops] fixed export css

--- scripts/copy-files.js
+++ scripts/copy-files.js
@@ -20,6 +20,19 @@ async function typesCopy({ from, to }) {
   return Promise.all(cmds);
 }
 
+async function cssCopy({ from, to }) {
+  if (!(await fse.exists(to))) {
+    console.warn(`path ${to} does not exists`);
+    return [];
+  }
+
+  const files = glob.sync('**/*.css', { cwd: from });
+  const cmds = files.map(async file => {
+    await fse.copy(path.resolve(from, file), path.resolve(to, file))
+  });
+  return Promise.all(cmds);
+}
+
 async function createPackageFile() {
   const packageData = await fse.readFile(path.resolve(packagePath, './package.json'), 'utf8');
   const { scripts, devDependencies, ...packageDataOther } = JSON.parse(
@@ -43,6 +56,7 @@ async function createPackageFile() {
 async function run() {
   await createPackageFile()
   await typesCopy({ from: srcPath, to: buildPath });
+  await cssCopy({ from: srcPath, to: buildPath });
 }
 
 run();
