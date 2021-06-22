a46456b9c7db9c5f511cac3521961fb6415841c7
SP_Penguin <abrcdf1023@gmail.com>
2021-06-22 15:06:05 +0800

    [utils] update roundToTwoDecimal test case

--- packages/utils/src/roundToTwoDecimal/roundToTwoDecimal.test.ts
+++ packages/utils/src/roundToTwoDecimal/roundToTwoDecimal.test.ts
@@ -6,5 +6,6 @@ describe('roundToTwoDecimal', () => {
     expect(roundToTwoDecimal(1.7777777)).toBe(1.78);
     expect(roundToTwoDecimal(9.1)).toBe(9.1);
     expect(roundToTwoDecimal(1.005)).toBe(1.01);
+    expect(roundToTwoDecimal(0.2833333333333333)).toBe(0.28);
   });
 });
