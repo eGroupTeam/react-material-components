bc77297f11f58a59508838a43c67a30edcbb5e3a
SP_Penguin <abrcdf1023@gmail.com>
2021-06-23 10:06:34 +0800

    [hooks] fixed useMediaPrint bug

--- packages/hooks/src/useMediaPrint.ts
+++ packages/hooks/src/useMediaPrint.ts
@@ -1,34 +1,40 @@
-import { useRef } from 'react';
+import { useCallback, useRef } from 'react';
 import { jsPDF as JsPDF } from 'jspdf';
 import pdfAddPages from '@e-group/utils/pdfAddPages';
 
 export default function useMediaPrint(printBoxId: string) {
   const printRefs = useRef<HTMLDivElement>(null);
 
-  const handlePrintPdf = async (filename?: string) => {
-    if (!printRefs.current) {
-      return;
-    }
-    const pdf = new JsPDF('p', 'mm', 'a4');
-    await pdfAddPages(printBoxId, pdf, [printRefs.current], {
-      xPadding: 8,
-      yPadding: 8,
-    });
-    pdf.save(filename);
-  };
+  const handlePrintPdf = useCallback(
+    async (filename?: string) => {
+      if (!printRefs.current) {
+        return;
+      }
+      const pdf = new JsPDF('p', 'mm', 'a4');
+      await pdfAddPages(printBoxId, pdf, [printRefs.current], {
+        xPadding: 8,
+        yPadding: 8,
+      });
+      pdf.save(filename);
+    },
+    [printBoxId],
+  )
 
-  const handleMediaPrint = () => {
-    const css = '@page { size: A4 portrait; }';
-    const head = document.head || document.getElementsByTagName('head')[0];
-    const style = document.createElement('style');
-
-    style.media = 'print';
-    style.appendChild(document.createTextNode(css));
-
-    head.appendChild(style);
-    window.print();
-    head.removeChild(style);
-  };
+  const handleMediaPrint = useCallback(
+    () => {
+      const css = '@page { size: A4 portrait; }';
+      const head = document.head || document.getElementsByTagName('head')[0];
+      const style = document.createElement('style');
+  
+      style.media = 'print';
+      style.appendChild(document.createTextNode(css));
+  
+      head.appendChild(style);
+      window.print();
+      head.removeChild(style);
+    },
+    [],
+  )
 
   return {
     handlePrintPdf,
